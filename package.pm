c70997a1aae6b2e4228e6c0bbfd5aa05bdc76a21
Andrew Klymenko <klymenka@ncbi.nlm.nih.gov>
2014-10-17 15:23:01 -0400

    1349: allow to specify custome path to 3-rd party libraries

--- package.pm
+++ package.pm
@@ -1,32 +1,36 @@
 ################################################################################
-sub PACKAGE      { 'sra-tools' }
-sub VERSION      { '2.4.2-1' }
+sub PACKAGE      { "sra-tools" }
+sub VERSION      { "2.4.2-1" }
 sub PACKAGE_TYPE { 'B' }
 sub PACKAGE_NAME { "SRA-TOOLS" }
 sub PACKAGE_NAMW { "SRATOOLS" }
-sub DEPENDS      { qw(hdf5 xml2 magic) }
 sub CONFIG_OUT   { 'build' }
 sub PKG { ( LNG   => 'C',
             OUT   => 'ncbi-outdir',
             PATH  => '/usr/local/ncbi/sra-tools',
             UPATH =>      '$HOME/ncbi/sra-tools', ) }
+sub DEPENDS      { ( { name => 'hdf5' , },
+                     { name => 'magic', Include => '/usr/include'        , },
+                     { name => 'xml2' , Include => '/usr/include/libxml2', } ) }
 sub REQ { (
             { name    => 'ngs-sdk',
               namew   => 'NGS',
               option  => 'with-ngs-sdk-prefix',
+              origin  => 'I',
               type    => 'L',
               srcpath => '../ngs/ngs-sdk',
               pkgpath => '/usr/local/ngs/ngs-sdk',
               usrpath =>      '$HOME/ngs/ngs-sdk',
               bldpath => '$HOME/ncbi-outdir/ngs-sdk',
               include => 'ngs/itf/Refcount.h',
-              lib     => 'libngs-sdk.$SHLX',
+              lib     => 'libngs-c++.a',
               ilib    => 'libngs-bind-c++.a',
             },
             { name    => 'ncbi-vdb',
               namew   => 'VDB',
               option  => 'with-ncbi-vdb-sources',
               boption => 'with-ncbi-vdb-build',
+              origin  => 'I',
               type    => 'SB',
               srcpath => '../ncbi-vdb',
               pkgpath => '/usr/local/ncbi/ncbi-vdb',
@@ -35,5 +39,14 @@ sub REQ { (
               include => 'klib/rc.h',
               lib     => 'libncbi-vdb.a',
               ilib    => 'libkapp.a',
+        },
+        {     name    => 'hdf5',
+              option  => 'with-hdf5-prefix',
+              origin  => 'E',
+              type    => 'LI',
+              pkgpath => '/usr',
+              usrpath => '$HOME',
+              include => 'hdf5.h',
+              lib => 'libhdf5.a',
         } ) }
 1

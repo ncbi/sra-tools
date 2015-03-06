d44cf33bc3686296b479acaebc03a708c2329091
ksrodarmer <rodarme1@ncbi.nlm.nih.gov>
2015-03-06 16:15:07 -0500

    added initial version of writer API

---
+++ tools/general-loader/general-writer.cpp
@@ -0,0 +1,371 @@
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+*/
+
+#include "general-writer.hpp"
+
+#include <iterator>
+#include <cstdlib>
+#include <unistd.h>
+
+#include <assert.h>
+#include <string.h>
+
+namespace ncbi
+{
+
+    int GeneralWriter :: addTable ( const std :: string &table_name )
+    {        
+        if ( isClosed )
+            throw "Cannot add tables after end-stream";
+
+        if ( isOpen )
+            throw "Cannot add tables after open";
+        
+        // prediction this is the index
+        int id = ( int ) table_names.size() + 1;
+
+        // make sure we never record a table name twice
+        std :: pair < std :: map < std :: string, int > :: iterator, bool > result = 
+            table_name_idx.insert ( std :: pair < std :: string, int > ( table_name, id ) );
+        
+        // if first time
+        if ( result.second )
+        {
+            table_names.push_back ( table_name );
+
+            
+            table_hdr hdr;
+            hdr.id = id;
+            hdr.evt = evt_new_table;
+            hdr.table_name_size = table_name.size();
+
+            // write header        
+            internal_write ( ( const char * ) &hdr, sizeof hdr );
+
+            // write out string data - NOT NUL TERMINATED!!
+            internal_write ( table_name.data(), hdr.table_name_size );
+
+            // force a NUL termination by writing 1..4 NUL bytes
+            static char zeros [ 4 ];
+            internal_write ( zeros, 4 - hdr.table_name_size % 4 );
+        }
+        
+        // 1 based table id
+        return result.first->second;
+    }
+    
+    int GeneralWriter :: addColumn ( int table_id,
+                             const std :: string &column_name )
+    {
+        if ( isClosed )
+            throw "Cannot add columns after end-stream";
+
+        if ( isOpen )
+            throw "Cannot add columns after open";
+        
+        if ( table_id <= 0 || ( size_t ) table_id > table_names.size () )
+            throw "Invalid table id";
+        
+        // the thing to insert into map
+        int_stream stream ( table_id, column_name );
+
+        // prediction this is the index
+        int id = ( int ) streams.size() + 1;
+
+        // make sure we never record a column-spec twice
+        std :: pair < std :: map < int_stream, int > :: iterator, bool > result =
+            column_name_idx.insert ( std :: pair < int_stream, int > ( stream, id ) );
+        
+        // if first time
+        if ( result.second )
+        {
+            streams.push_back ( stream );
+
+            // TBD - write new column stream event to stream
+            column_hdr hdr;
+            hdr.id = id;
+            hdr.evt = evt_new_column;
+            hdr.table_id = table_id;
+            hdr.column_name_size = column_name.size ();
+
+            // write header        
+            internal_write ( ( const char * ) &hdr, sizeof hdr );
+
+            // write out string data - NOT NUL TERMINATED!!
+            internal_write ( column_name.data (), hdr.column_name_size );
+
+            // force a NUL termination by writing 1..4 NUL bytes
+            static char zeros [ 4 ];
+            internal_write ( zeros, 4 - hdr.column_name_size % 4 );
+        }
+        
+        // 1 based stream id
+        return result.first->second;
+    }
+ 
+    
+    void GeneralWriter :: open ()
+    {
+        if ( isClosed )
+            throw "Cannot open after end-stream";
+
+        if ( ! isOpen )
+        {
+            if ( table_names.size () == 0 )
+                throw "No tables have been added";
+            if ( streams.size () == 0 )
+                throw "No columns have been added";
+
+            isOpen = true;        
+
+            evt_hdr hdr;
+            hdr.id = 0;
+            hdr.evt = evt_open_stream;
+
+            // write header        
+            internal_write ( ( const char * ) &hdr, sizeof hdr );
+        }
+    }
+
+    
+    void GeneralWriter :: columnDefault ( int stream_id, uint32_t elem_bits, const void *data, uint32_t elem_count )
+    {
+        if ( stream_id < 0 )
+            throw "Stream_id is not valid";
+        if ( stream_id > ( int ) streams.size () )
+            throw "Stream_id is out of bounds";
+        
+        if ( isClosed )
+            throw "Cannot set column defaults after end-stream";
+
+        if ( ! isOpen )
+            throw "Cannot set column defaults before open";
+
+        if ( elem_bits == 0 || elem_count == 0 )
+            return;
+        
+        if ( data == 0 )
+            throw "Invalid data ptr";
+        
+        cell_hdr chunk;
+        
+        assert ( stream_id <= 0x00FFFFFF );
+
+        chunk.id = stream_id;
+        chunk.evt = evt_cell_default;
+        chunk.elem_bits = elem_bits;
+        chunk.elem_count = elem_count;
+
+        size_t num_bytes = ( size_t ) ( ( uint64_t ) elem_bits * elem_count + 7 ) / 8;
+        
+        internal_write ( ( const char * ) &chunk, sizeof chunk );
+        internal_write ( ( const char * ) data, num_bytes );
+        
+        if ( num_bytes % 4 != 0 )
+        {
+            static char zeros [ 4 ];
+            internal_write ( zeros, 4 - num_bytes % 4 );
+        }
+
+    }
+
+    void GeneralWriter :: write ( int stream_id, uint32_t elem_bits, const void *data, uint32_t elem_count )
+    {
+        if ( stream_id < 0 )
+            throw "Stream_id is not valid";
+        if ( stream_id > ( int ) streams.size () )
+            throw "Stream_id is out of bounds";
+        
+        if ( isClosed )
+            throw "Cannot write column data after end-stream";
+
+        if ( ! isOpen )
+            throw "Cannot write column data before open";
+        
+        if ( elem_bits == 0 || elem_count == 0 )
+            return;
+        
+        if ( data == 0 )
+            throw "Invalid data ptr";
+        
+        cell_hdr chunk;
+        
+        assert ( stream_id <= 0x00FFFFFF );
+
+        chunk.id = stream_id;
+        chunk.evt = evt_cell_data;
+        chunk.elem_bits = elem_bits;
+        chunk.elem_count = elem_count;
+
+        size_t num_bytes = ( size_t ) ( ( uint64_t ) elem_bits * elem_count + 7 ) / 8;
+        
+        internal_write ( ( const char * ) &chunk, sizeof chunk );
+        internal_write ( ( const char * ) data, num_bytes );
+        
+        if ( num_bytes % 4 != 0 )
+        {
+            static char zeros [ 4 ];
+            internal_write ( zeros, 4 - num_bytes % 4 );
+        }
+
+    }
+
+    void GeneralWriter :: nextRow ( int table_id )
+    {
+        if ( isClosed )
+            throw "Cannot commit table after end-stream";
+
+        if ( ! isOpen )
+            throw "Cannot commit table before open";
+        
+        if ( table_id < 0 || ( size_t ) table_id > table_names.size () )
+            throw "Invalid table id";
+
+        evt_hdr hdr;
+
+        assert ( table_id <= 0x00FFFFFF );
+
+        hdr.id = table_id;
+        hdr.evt = evt_next_row;
+
+        internal_write ( ( const char * ) &hdr, sizeof hdr );
+    }
+
+    void GeneralWriter :: endStream ()
+    {
+        if ( ! isClosed )
+        {
+            evt_hdr hdr;
+
+            hdr.id = 0;
+            hdr.evt = evt_end_stream;
+
+            internal_write ( ( const char * ) &hdr, sizeof hdr );
+
+            isClosed = true;
+        }
+    }
+
+    
+    // Constructors
+    GeneralWriter :: GeneralWriter ( const std :: string &out_path,
+                             const std :: string &remote_db,
+                             const std :: string &schema_file_name, 
+                             const std :: string &schema_db_spec )
+    : out ( out_path.c_str(), std::ofstream::binary )
+    , remote_db ( remote_db )
+    , schema_file_name ( schema_file_name )
+    , schema_db_spec ( schema_db_spec )
+    , out_fd ( -1 )
+    , isOpen ( false )
+    , isClosed ( false )
+    {
+        writeHeader ();
+
+    }
+
+    
+    // Constructors
+    GeneralWriter :: GeneralWriter ( int _out_fd,
+                             const std :: string &remote_db,
+                             const std :: string &schema_file_name, 
+                             const std :: string &schema_db_spec )
+    : remote_db ( remote_db )
+    , schema_file_name ( schema_file_name )
+    , schema_db_spec ( schema_db_spec )
+    , out_fd ( _out_fd )
+    , isOpen ( false )
+    , isClosed ( false )
+    {
+        writeHeader ();
+    }
+    
+    GeneralWriter :: ~GeneralWriter ()
+    {
+        endStream ();
+        out.flush ();
+    }
+
+    bool GeneralWriter :: int_stream :: operator < ( const int_stream &s ) const
+    {
+        if ( table_id != s.table_id )
+            return table_id < s.table_id;
+        return column_name.compare ( s.column_name ) < 0;
+    }
+    
+    GeneralWriter :: int_stream :: int_stream ( int _table_id, const std :: string &_column_name )
+    : table_id ( _table_id )
+    , column_name ( _column_name )
+    {
+        
+    }
+
+    // Private methods
+
+    void GeneralWriter :: writeHeader ()
+    {
+        header hdr;
+        memcpy ( hdr.signature, "NCBIgnld", sizeof hdr.signature ); // replace with constant
+        hdr.endian = 1;         // replace "1" with a constant from include file
+        hdr.version = 1;        // replace with constant from current version
+        hdr.remote_db_name_size = remote_db.size ();
+        hdr.schema_file_name_size = schema_file_name.size ();
+        hdr.schema_db_spec_size = schema_db_spec.size ();
+
+        internal_write ( ( const char * ) &hdr, sizeof hdr );
+        internal_write ( remote_db.c_str (), hdr.remote_db_name_size + 1 );
+        internal_write ( schema_file_name.c_str (), hdr.schema_file_name_size + 1 );
+        internal_write ( schema_db_spec.c_str (), hdr.schema_db_spec_size + 1 );
+
+        size_t num_bytes =  hdr.remote_db_name_size + hdr.schema_file_name_size + hdr.schema_db_spec_size + 3;
+
+        if ( num_bytes % 4 != 0 )
+        {
+            static char zeros [ 4 ];
+            internal_write ( zeros, 4 - num_bytes % 4 );
+        }
+
+    }
+
+    void GeneralWriter :: internal_write ( const void *data, size_t num_bytes )
+    {
+        if ( out_fd < 0 )
+            out.write ( ( const char * ) data, num_bytes );
+        else
+        {
+            for ( size_t total = 0; total < num_bytes; )
+            {
+                ssize_t num_writ = :: write ( out_fd, ( void * ) & ( ( char * ) data ) [ total ], num_bytes - total );
+                if ( num_writ < 0 )
+                    throw "Error writing to fd";
+                if ( num_writ == 0 )
+                    throw "Transfer incomplete writing to fd";
+                total += num_writ;
+            }
+        }
+    }
+
+}
---
+++ tools/general-loader/general-writer.hpp
@@ -0,0 +1,171 @@
+/*===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+*/
+
+#ifndef _hpp_general_writer_
+#define _hpp_general_writer_
+
+#include <iostream>
+#include <string>
+#include <stdint.h>
+#include <vector>
+#include <fstream>
+#include <map>
+
+namespace ncbi
+{
+    class GeneralWriter
+    {
+    public:
+
+        int addTable ( const std :: string &table_name );
+
+        int addColumn ( int table_id, const std :: string &column_name );
+
+        // ensure there are atleast one table and one column
+        // set GeneralWriter to open state
+        // write out open_stream event header
+        void open ();
+
+        // generates a chunk of cell data
+        // MUST be entire default value in one event
+        void  columnDefault ( int stream_id, uint32_t elem_bits, const void *data, uint32_t elem_count );
+
+        // generate a chunk of cell data
+        // may be repeated as often as necessary to complete a single cell's data
+        void write ( int stream_id, uint32_t elem_bits, const void *data, uint32_t elem_count );
+
+        // generate an event
+        void nextRow ( int table_id );
+
+        // generates an end event
+        // puts object into state that will reject any further transmissions
+        void endStream ();
+
+        // out_path initializes output stream 
+        // schema_name is recorded for the open
+        GeneralWriter ( const std :: string &out_path,
+                    const std :: string &remote_db,
+                    const std :: string &schema_file_name, 
+                    const std :: string &schema_db_spec );
+
+        GeneralWriter ( int out_fd,
+                    const std :: string &remote_db,
+                    const std :: string &schema_file_name, 
+                    const std :: string &schema_db_spec );
+
+
+        // output stream is flushed and closed
+        ~ GeneralWriter ();
+
+    private:
+
+        void writeHeader ();
+        void internal_write ( const void *data, size_t num_bytes );
+
+        struct header
+        {
+            char signature [ 8 ];   // 8 characters to identify file type
+            uint32_t endian;        // an internally known pattern to identify endian
+            uint32_t version;          // a single-integer version number
+
+            uint32_t remote_db_name_size;
+            uint32_t schema_file_name_size;
+            uint32_t schema_db_spec_size;
+            // string data follows: 3 strings plus 1 NUL byte for each, 4-byte aligned
+            // uint32_t data [ ( ( remote_db_name_size + schema_file_name_size + schema_spec_size + 3 ) + 3 ) / 4 ];
+        };
+
+        enum evt_id
+        {
+            evt_end_stream,
+            evt_new_table,
+            evt_new_column,
+            evt_open_stream,
+            evt_cell_default, 
+            evt_cell_data, 
+            evt_next_row 
+        };
+
+        struct table_hdr
+        {
+            uint32_t id : 24;
+            uint32_t evt : 8;
+            uint32_t table_name_size;
+            // uint32_t data [ ( ( table_name_size + 1 ) + 3 ) / 4 ];
+        };
+
+        struct column_hdr
+        {
+            uint32_t id : 24;
+            uint32_t evt : 8;
+            uint32_t table_id;
+            uint32_t column_name_size;
+            // uint32_t data [ ( ( column_name_size + 1 ) + 3 ) / 4 ];
+        };
+
+        struct cell_hdr
+        {
+            uint32_t id : 24;
+            uint32_t evt : 8;
+            uint32_t elem_bits;
+            uint32_t elem_count;
+            // uint32_t data [ ( elem_bits * elem_count + 31 ) / 32 ];
+        };
+
+        struct evt_hdr
+        {
+            uint32_t id : 24;
+            uint32_t evt : 8;
+        };
+
+        struct int_stream
+        {
+            bool operator < ( const int_stream &s ) const;
+            int_stream ( int table_id, const std :: string &column_name );
+
+            int table_id;
+            std :: string column_name;
+        };
+
+        std :: ofstream out;
+
+        std :: string remote_db;
+        std :: string schema_file_name;
+        std :: string schema_db_spec;
+
+        std :: map < std :: string, int > table_name_idx;
+        std :: map < int_stream, int > column_name_idx;
+
+        std :: vector < int_stream > streams;
+        std :: vector < std :: string > table_names;
+
+        int out_fd;
+        bool isOpen;
+        bool isClosed;
+    };
+}
+
+#endif // _hpp_general_writer_

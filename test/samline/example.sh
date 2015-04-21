49ce78c6f32c0e2b04443838520a422323daa69d
wraetz <raetzw@ncbi.nlm.nih.gov>
2015-04-21 13:51:20 -0400

    feed bamload with BAM not SAM

--- test/samline/example.sh
+++ test/samline/example.sh
@@ -8,29 +8,52 @@ execute()
     echo "."
 }
 
-#produce 1 alignment against reference NC_011752.1 at position 1000 (1based) with cigar-string 50M
-REFNAME="NC_011752.1"
-REFPOS1=1000
-REFPOS2=6000
-CIGAR1="30MAAA20M"
-CIGAR2="100M"
-CONFIG="config.kfg"
+# call: produce_SAM "$CONFIG" "$SAMFILE"
+produce_SAM()
+{
+	SAMLINE_BINARY="samline"
+	REFNAME="NC_011752.1"
+	REFPOS1=1000
+	REFPOS2=6000
+	CIGAR1="30MAAA20M"
+	CIGAR2="100M"
+	execute "$SAMLINE_BINARY -r $REFNAME -p $REFPOS1 -p $REFPOS2 -c $CIGAR1 -c $CIGAR2 -n $1 -d > $2"
+}
 
-SAMLINE="samline -r $REFNAME -p $REFPOS1 -p $REFPOS2 -c $CIGAR1 -c $CIGAR2 -n $CONFIG -d"
+# call: convert_SAM_to_BAM "$SAMFILE" "$BAMFILE"
+convert_SAM_to_BAM()
+{
+	SAMTOOLS_BINARY="/netopt/ncbi_tools64/samtools/bin/samtools"
+	execute "$SAMTOOLS_BINARY view -bS $1 > $2"
+}
 
-#load this alignment with bam-load
-OUTDIR="test"
-LOADLINE="bam-load -o $OUTDIR -k $CONFIG -E0 -Q0 /dev/stdin"
+# call: load_BAM_to_CSRA "$CONFIG" "$BAMFILE" "$TEMP_DIR"
+load_BAM_to_CSRA()
+{
+	BAMLOAD_BINARY="bam-load"
+	execute "$BAMLOAD_BINARY -L 3 -o $3 -k $1 -E0 -Q0 $2"
+}
 
-#kar it up
-OUTFILE="test.sra"
-KARLINE="kar --create $OUTFILE -d $OUTDIR -f"
+# call: kar_CSRA "$FINAL_CSRA" "$TEMP_DIR"
+kar_CSRA()
+{
+	KAR_BINARY="kar"
+	execute "$KAR_BINARY --create $1 -d $2 -f"
+}
+
+SAMFILE="temp.SAM"
+BAMFILE="temp.BAM"
+CONFIG="temp.kfg"
+TEMP_DIR="temp_csra"
+FINAL_CSRA="test.csra"
 
 #print vdb-dump to show success
-DUMPLINE="vdb-dump $OUTFILE --info"
+SHOW_RESULT="vdb-dump $OUTFILE --info"
 
-#remove temp. directory
-RMLINE="rm -rf $OUTDIR"
+produce_SAM "$CONFIG" "$SAMFILE"
+convert_SAM_to_BAM "$SAMFILE" "$BAMFILE"
+load_BAM_to_CSRA "$CONFIG" "$BAMFILE" "$TEMP_DIR"
+kar_CSRA "$FINAL_CSRA" "$TEMP_DIR"
 
-#execute "$SAMLINE"
-execute "$SAMLINE | $LOADLINE && $KARLINE && $DUMPLINE && $RMLINE"
+execute "rm -rf $TEMP_DIR $SAMFILE $BAMFILE $CONFIG"
+execute "vdb-dump $FINAL_CSRA --info"

a325f06ab900765662faf33383479686c467738d
boshkina <boshkina@nih.gov>
2015-07-14 14:42:13 -0400

    VDB-1345: a simplified Windows script (Ant/.bat) for testing of tarballs

--- test/tarballs/test-tarballs.bat
+++ test/tarballs/test-tarballs.bat
@@ -22,31 +22,20 @@
 ::
 :: ===========================================================================
 
-echo off
+echo on
 Setlocal
 Setlocal EnableDelayedExpansion
 
-set TOOLS=abi-dump align-info blastn_vdb cache-mgr fastq-dump illumina-dump kar kdbmeta latf-load prefetch rcexplain sam-dump sff-dump ^
+set TOOLS=abi-dump blastn_vdb cache-mgr fastq-dump illumina-dump kar kdbmeta latf-load prefetch rcexplain sam-dump sff-dump ^
           sra-kar sra-pileup sra-stat srapath tblastn_vdb test-sra vdb-config vdb-copy vdb-decrypt vdb-dump vdb-encrypt vdb-lock ^
           vdb-unlock vdb-validate   
 
+:: align-info throws a crash dialog, disable for now
 :: vdb-passwd is obsolete but still in the package
-
-:: this is done from TeamCity
-:: powershell -Command wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-win64.zip -Outfile sratoolkit.current-win64.zip || exit /b 1
-
-jar xf sratoolkit.current-win64.zip || exit /b 2
-
-cd sratoolkit.2.5.2-win64\bin
 
 set FAILED=
 
-for %%t in ( %TOOLS% ) do ( echo | set /p=%%t & %%t -h >NUL & ( if errorlevel 1 ( echo | set /p=failed! & set FAILED=!FAILED! %%t ) ) & echo. )
+for %%t in ( %TOOLS% ) do ( echo | set /p=%%t & %1\%%t -h >NUL & ( if errorlevel 1 ( echo | set /p=failed! & set FAILED=!FAILED! %%t ) ) & echo. )
 
-if "%FAILED%" NEQ "" ( echo. & echo FAILED: %FAILED% & exit /b 3 )
+if "%FAILED%" NEQ "" ( echo. & echo FAILED: %FAILED% & exit /b 1 )
 
-cd ..\..
-rmdir /S /Q sratoolkit.2.5.2-win64
-del /Q sratoolkit.current-win64.zip
-
-exit /b 0

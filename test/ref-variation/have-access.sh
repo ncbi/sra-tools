0af386869305c05f69b394cc23031246b9356d64
alexukr <ukrainch@ncbi.nlm.nih.gov>
2016-02-26 18:44:49 -0500

    VDB-2748 turned off ref-variation test if there is no access to the repository - fix

--- test/ref-variation/have-access.sh
+++ test/ref-variation/have-access.sh
@@ -1,38 +1,25 @@
 BINDIR=$1
-mode=$2
-
+test_name=$2
+exit_code=0
 have_access()
 {
 	$BINDIR/vdb-config -on /repository/site > /dev/null
 }
 
-var_expand()
-{
-	if have_access; then
-		echo "Starting var-expand test...";
-		rm -f var-expand.out;
-		echo "a CM000664.1:234668879:14:AT" | $BINDIR/var-expand --algorithm=ra >> var-expand.out;
-		diff expected/var-expand.out var-expand.out;
-		rm var-expand.out;
-		echo "var-expand test is done";
-	else
-		echo "no access to repository, skipping var-expand test...";
-	fi
-}
-
-ref_variation()
-{
-	if have_access; then 
-		echo "Starting ref-variation test..."; 
+if have_access; then
+	echo "Starting $test_name test..."; 
+	if [ "$test_name" = "ref-variation" ]; then
 		./ref-variation.sh $BINDIR; 
-		echo "ref-variation test is done";	
-	else 
-		echo "no access to repository, skipping ref-variation test..."; 
+		exit_code=$?
+	elif [ "$test_name" = "var-expand" ]; then
+		rm -f var-expand.out
+		echo "a CM000664.1:234668879:14:AT" | $BINDIR/var-expand --algorithm=ra >> var-expand.out
+		diff expected/var-expand.out var-expand.out
+		exit_code=$?
+		rm var-expand.out
 	fi
-}
-
-if [ "$mode" = "ref-variation" ]; then
-	ref_variation
-elif [ "$mode" = "var-expand" ]; then
-	var_expand
+	echo "$test_name test is done";	
+else
+	echo "no access to repository, skipping $test_name test...";
 fi
+exit $exit_code

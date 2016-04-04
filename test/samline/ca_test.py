9d8f7d9e20fbdf8f58694320a4c7215afbd54d27
wraetz <raetzw@ncbi.nlm.nih.gov>
2016-04-04 16:16:44 -0400

    added explanations to the 3 tests in here

--- test/samline/ca_test.py
+++ test/samline/ca_test.py
@@ -2,25 +2,30 @@
 from sam import *
 
 def dump( acc ) :
-    print "SEQ"
-    vdb_dump( acc, "-C READ -l0" )
-    print "PRIM"
-    vdb_dump( acc, "-T PRIMARY_ALIGNMENT -C READ -l0" )
-    print "SEC"
-    vdb_dump( acc, "-T SECONDARY_ALIGNMENT -C READ -l0" )
+    print "%s.SEQ"%(acc)
+    vdb_dump( acc, "-C SPOT_ID,READ -l0 -f tab" )
+    print "%s.PRIM"%( acc )
+    vdb_dump( acc, "-T PRIMARY_ALIGNMENT -C ALIGN_ID,READ -l0, -f tab" )
+    print "%s.SEC"%( acc )
+    vdb_dump( acc, "-T SECONDARY_ALIGNMENT -C ALIGN_ID,READ -l0 -f tab" )
 
 REF   = "NC_011752.1"
 ALIAS = "c1"
-CSRA1 = "X.CSRA"
-CSRA2 = "S.CSRA"
+CSRA1 = "AFTER_BAM_LOAD.CSRA"
+CSRA2 = "AFTER_SRA_SORT.CSRA"
 
 def load_sort( L ) :
     R1 = bam_load( L, CSRA1, "--make-spots-with-secondary -L 3 -E0 -Q0" )
-    print "bam-load = %d"%( R1 )
     if R1 == 1 :
-        R2 = sra_sort( CSRA1, CSRA2, "-vvvv" )
-        print "sra-sort = %d"%( R2 )
-
+        print "bam-load = OK"
+        R2 = sra_sort( CSRA1, CSRA2 )
+        if R2 == 1 :
+            print "sra-sort = OK"
+        else :
+            print "sra-sort = FAILED"
+    else :
+        print "bam-load = FAILED"
+        
 def load_sort_print( L ) :
     load_sort( L )
     dump( CSRA1 )
@@ -28,6 +33,8 @@ def load_sort_print( L ) :
 
 
 def test1() :
+    print "test #1 --------------------------------------------------"
+    print "...having a single recondary alignmnet without a primary it belongs to"
     A1 = make_prim( "A1", 0, REF, ALIAS, 17000, 20, "60M" )
     A2 = make_prim( "A2", 0, REF, ALIAS, 12500, 20, "50M" )
     A1.pair_with( A2 )
@@ -44,6 +51,9 @@ def test1() :
 
 # the resulting X.CSRA and S.CSRA produce errors in seq_restore_read_impl2
 def test2() :
+    print "test #2 --------------------------------------------------"
+    print "...having a pair of a prim. and a sec. alignment"
+    print "= SEQUENCE-table cannot reconstruct READ"
     A1 = make_prim( "A1", 0, REF, ALIAS, 17000, 20, "60M" )
     A2 = make_sec( "A2", 0, REF, ALIAS, 12500, 20, "50M" )
     A1.pair_with( A2 )
@@ -53,6 +63,7 @@ def test2() :
 # the resulting X.CSRA produces errors in seq_restore_read_impl2
 # but S.CSRA sefaults in vdb-dump!
 def test3() :
+    print "test #3 --------------------------------------------------"
     A1 = make_prim( "A1", 0, REF, ALIAS, 1000, 20, "53M" )
     A2 = make_sec( "A2", 0, REF, ALIAS, 3500, 20, "50M" )
     A1.pair_with( A2 )
@@ -60,17 +71,10 @@ def test3() :
     A3 = make_sec( "A3", 0, REF, ALIAS, 6800, 20, "55M" )
     A3.flags |= FLAG_NEXT_UNMAPPED
 
-    load_sort( [ A3, A1, A2 ] )
+    load_sort_print( [ A3, A1, A2 ] )
 
-# the resulting X.CSRA produces errors in seq_restore_read_impl2
-def test4() :
-    A1 = make_prim( "A1", 0, REF, ALIAS, 1000, 20, "53M" )
-    A2 = make_sec( "A2", 0, REF, ALIAS, 3500, 20, "50M" )
-    A1.pair_with( A2 )
 
-    A3 = make_sec( "A3", 0, REF, ALIAS, 6800, 20, "55M" )
-    A3.flags |= FLAG_NEXT_UNMAPPED
-
-    load_sort( [ A1, A2, A3 ] )
-
+test1()
+test2()
 test3()
+

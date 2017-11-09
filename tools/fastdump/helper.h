545f0baaa1e8abc0e3f1018b46372b91b670929a
wraetz <raetzw@ncbi.nlm.nih.gov>
2017-11-09 13:38:12 -0500

    return type fixed for total_size_of_files_in_list()

--- tools/fastdump/helper.h
+++ tools/fastdump/helper.h
@@ -150,7 +150,7 @@ bool file_exists( const KDirectory * dir, const char * fmt, ... );
 void join_and_release_threads( Vector * threads );
 
 rc_t delete_files( KDirectory * dir, const VNamelist * files );
-rc_t total_size_of_files_in_list( KDirectory * dir, const VNamelist * files );
+uint64_t total_size_of_files_in_list( KDirectory * dir, const VNamelist * files );
 
 int get_vdb_pathtype( KDirectory * dir, const char * accession );
 

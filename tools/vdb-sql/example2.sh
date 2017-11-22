2022948e49e68db53b2e249769523cd1fd2fed28
wraetz <raetzw@ncbi.nlm.nih.gov>
2017-11-22 15:30:43 -0500

    fix to the examples - the tool does not need to be located any more

--- tools/vdb-sql/example2.sh
+++ tools/vdb-sql/example2.sh
@@ -1,24 +1,6 @@
 echo "----- list of References -----"
 
-TOOL="../../../OUTDIR/sra-tools/linux/gcc/x86_64/dbg/bin/vdb-sql"
-
-if [ ! -f $TOOL ]; then
-TOOL="../../../OUTDIR/sra-tools/linux/gcc/x86_64/rel/bin/vdb-sql"
-fi
-
-if [ ! -f $TOOL ]; then
-TOOL="../../../OUTDIR/sra-tools/mac/clang/x86_64/dbg/bin/vdb-sql"
-fi
-
-if [ ! -f $TOOL ]; then
-TOOL="../../../OUTDIR/sra-tools/mac/clang/x86_64/rel/bin/vdb-sql"
-fi
-
-if [ ! -f $TOOL ]; then
-    echo "tool not found"
-    exit 1
-fi
-
+TOOL="vdb-sql"
 ACC="SRR576646"
 SELECT="select SEQ_ID, NAME, count( SEQ_ID ), sum( READ_LEN ) from VDB group by SEQ_ID;"
 

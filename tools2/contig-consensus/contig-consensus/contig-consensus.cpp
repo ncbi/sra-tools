fbf5d115b27ddd4bd44424f39daee8466deac4f0
durbrowk@ncbi.nlm.nih.gov <kmdurbrow@me.com>
2017-12-05 12:33:24 -0500

    New project

---
+++ tools2/contig-consensus/contig-consensus/contig-consensus.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  contig-consensus
+//
+//  Created by Kenneth Durbrow on 12/4/17.
+//  Copyright © 2017 NCBI/NLM/NIH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

4065f3352807164f8f0d00a3c8ef17c42b7589d3
klymenko <klymenka@mail.nih.gov>
2018-05-09 13:55:57 -0400

    VDB-3588: check what is in WORKDIR

--- test/tarballs/test-tarballs.sh
+++ test/tarballs/test-tarballs.sh
@@ -134,4 +134,4 @@ echo rm ${TK_PACKAGE} ${TK_TARGET}.tar ${GATK_TARGET} \
             ${NGS_PACKAGE} ${NGS_TARGET}.tar
 rm -rf  ${TK_PACKAGE} ${TK_TARGET}.tar ${GATK_TARGET} \
             ${NGS_PACKAGE} ${NGS_TARGET}.tar
-cd ${OLDDIR} && rmdir ${WORKDIR}
+cd ${OLDDIR} && ls ${WORKDIR} && rmdir ${WORKDIR}

8ab604446c6018866edad0b811c61b2167b8606e
durbrowk@ncbi.nlm.nih.gov <kmdurbrow@me.com>
2019-03-11 14:37:06 -0400

    Refactored parsing Introduced enum for opcodes

--- tools2/shared/include/CIGAR.hpp
+++ tools2/shared/include/CIGAR.hpp
@@ -37,6 +37,56 @@
 struct CIGAR_OP {
     uint32_t value; ///< equivalent to BAM encoding
     
+    enum Operation {
+        match,
+        insertion,
+        deletion,
+        intron,
+        softClip,
+        hardClip,
+        padding,
+        equals,
+        notEquals,
+        back
+    };
+private:
+    explicit CIGAR_OP(uint32_t value) : value(value) {}
+    static uint16_t constexpr matchMask() {
+        return (1 << match) | (1 << equals) | (1 << notEquals);
+    }
+    static uint16_t constexpr QBits() {
+        return matchMask() | (1 << insertion) | (1 << softClip) | (1 << back);
+    }
+    static uint16_t constexpr RBits() {
+        return matchMask() | (1 << deletion) | (1 << intron);
+    }
+    uint16_t opBit() const {
+        return (value & 0x0F) <= back ? (1 << (value & 0x0F)) : 0;
+    }
+public:
+    CIGAR_OP(int length, Operation op) : value(uint32_t(length << 4) | uint32_t(op)) {}
+    bool isa(Operation op) const {
+        return (value & 0x0F) == op;
+    }
+    static CIGAR_OP invalid() {
+        return CIGAR_OP(UINT32_MAX);
+    }
+    operator bool() const { return opBit() != 0; }
+    bool isMatch() const {
+        return (opBit() & matchMask()) != 0;
+    }
+    CIGAR_OP changeOperation(Operation op) const {
+        auto const orig_op = value & 0x0F;
+        return CIGAR_OP(value ^ orig_op ^ op);
+    }
+    bool isSameOperation(CIGAR_OP const &other) const {
+        return (value & 0x0F) == (other.value & 0x0F);
+    }
+    CIGAR_OP merge(CIGAR_OP const &other) const {
+        auto const op = value & 0x0F;
+        assert(op == (other.value & 0x0F));
+        return CIGAR_OP(((value ^ op) + (other.value ^ op)) ^ op);
+    }
     unsigned length() const ///< extract the operation length (or count)
     {
         return value >> 4;
@@ -47,89 +97,72 @@ struct CIGAR_OP {
     }
     unsigned qlength() const ///< effects how many bases of the query sequence
     {
-        switch (value & 15) {
-            case 0:
-            case 1:
-            case 4:
-            case 7:
-            case 8:
-            case 9:
-                return length();
-            default:
-                return 0;
-        }
+        return (opBit() & QBits()) != 0 ? length() : 0;
     }
     unsigned rlength() const ///< effects how many bases of the reference sequence
     {
-        switch (value & 15) {
-            case 0:
-            case 2:
-            case 3:
-            case 7:
-            case 8:
-                return length();
-            default:
-                return 0;
+        return (opBit() & RBits()) != 0 ? length() : 0;
+    }
+
+    void appendToString(std::string &out) const {
+        if ((value & 0x0F) <= back && (value >> 4) > 0) {
+            char buffer[16] = { '\0' };
+            auto cp = buffer + 15;
+            auto const endp = cp;
+            auto len = length();
+            
+            *--cp = opcode();
+            do { *--cp = '0' + (len % 10); len /= 10; } while (len > 0);
+            out.reserve(out.size() + (endp - cp));
+            out.append(cp, endp);
         }
+    }
+    std::string toString() const {
+        auto result = std::string();
+        appendToString(result);
+        return result;
     }
     
-    /** \brief parse a single operation
-     * \param str the string to be parsed
-     * \param offset the first character to be parsed; the value is in/out
-     * \returns length and opcode
-     */
-    static std::pair<int, int> parse(std::string const &str, unsigned &offset)
-    {
-        auto const N = str.length();
-        int length = 0;
-        int opcode = 0;
+    class Parser {
+    public:
+        using Iterator = std::string::const_iterator;
+    private:
+        Iterator pos;
+        Iterator const end;
+    public:
+        Parser(Iterator const &beg, Iterator const &end)
+        : pos(beg), end(end) {}
         
-        while (offset < N) {
-            opcode = str[offset++];
-            if (opcode < '0' || opcode > '9')
-                break;
-            length = length * 10 + (opcode - '0');
-            opcode = 0;
+        bool atEnd() const { return pos == end; }
+        CIGAR_OP next() {
+            if (pos == end) return invalid();
+
+            uint32_t length = 0;
+            
+            while (!atEnd()) {
+                int const ch = *pos++;
+                if (ch >= '0' && ch <= '9') {
+                    length = length * 10 + (ch - '0');
+                    continue;
+                }
+                switch (ch) {
+                    case 'M': return CIGAR_OP(length, match);
+                    case 'I': return CIGAR_OP(length, insertion);
+                    case 'D': return CIGAR_OP(length, deletion);
+                    case 'N': return CIGAR_OP(length, intron);
+                    case 'S': return CIGAR_OP(length, softClip);
+                    case 'H': return CIGAR_OP(length, hardClip);
+                    case 'P': return CIGAR_OP(length, padding);
+                    case '=': return CIGAR_OP(length, equals);
+                    case 'X': return CIGAR_OP(length, notEquals);
+                    case 'B': return CIGAR_OP(length, back);
+                    default:
+                        throw std::domain_error("invalid opcode");
+                }
+            }
+            throw std::domain_error("truncated CIGAR");
         }
-        return std::make_pair(length, opcode);
-    }
-    static std::string makeString(int length, int const opcode) ///< convert to string
-    {
-        char buffer[16];
-        char *cp = buffer + 16;
-        
-        if (length == 0) return std::string();
-        
-        *--cp = '\0';
-        *--cp = opcode;
-        while (length > 0) {
-            auto const ch = (length % 10) + '0';
-            *--cp = ch;
-            length /= 10;
-        }
-        return std::string(cp);
-    }
-    /** \brief construct a new value
-     * \param in the pair of length, opcode like as returned by parse
-     * \returns a new value or a '0' value
-     */
-    static CIGAR_OP compose(std::pair<int, int> const &in)
-    {
-        CIGAR_OP rslt; rslt.value = 0; ///< an erroneous value
-        switch (in.second) {
-            case 'M': rslt.value = (in.first << 4) | 0; break;
-            case 'I': rslt.value = (in.first << 4) | 1; break;
-            case 'D': rslt.value = (in.first << 4) | 2; break;
-            case 'N': rslt.value = (in.first << 4) | 3; break;
-            case 'S': rslt.value = (in.first << 4) | 4; break;
-            case 'H': rslt.value = (in.first << 4) | 5; break;
-            case 'P': rslt.value = (in.first << 4) | 6; break;
-            case '=': rslt.value = (in.first << 4) | 7; break;
-            case 'X': rslt.value = (in.first << 4) | 8; break;
-            case 'B': rslt.value = (in.first << 4) | 9; break;
-        }
-        return rslt;
-    }
+    };
 };
 
 /** \class CIGAR
@@ -155,6 +188,92 @@ private:
     , qclip(right_clip)
     , std::vector<CIGAR_OP>(other)
     {}
+    
+    using ParseResult = std::pair<std::pair<unsigned, unsigned>, std::vector<CIGAR_OP>>;
+    static ParseResult parse(std::string const &cigar) {
+        auto clip = std::pair<unsigned, unsigned>(0,0);
+        auto v = std::vector<CIGAR_OP>();
+        auto parser = CIGAR_OP::Parser(cigar.begin(), cigar.end());
+        auto count = 0;
+        auto hasLeftClip = false;
+        auto hasRightClip = false;
+        auto countNotClip = 0;
+
+        /* ([1-9][0-9]*H){0,1} #left hard clip
+         * ([1-9][0-9]*S){0,1} #left soft clip
+         * ([1-9][0-9]*[MIDNPX=])+ #regular CIGAR operations
+         * ([1-9][0-9]*S){0,1} #right soft clip
+         * ([1-9][0-9]*H){0,1} #right hard clip
+         */
+        while (auto op = parser.next()) {
+            count += 1;
+            if (op.isa(CIGAR_OP::hardClip)) {
+                if (count == 1) continue; ///< it is the left hard clip
+                break;                    ///< it is the right hard clip and should be the last operation
+            }
+            if (hasRightClip)
+                throw std::domain_error("bad soft clip");
+            if (op.isa(CIGAR_OP::softClip)) {
+                if (hasLeftClip) {
+                    clip.second = op.length();
+                    hasRightClip = true;
+                    continue;
+                }
+                if (countNotClip == 0) {
+                    clip.first = op.length();
+                    hasLeftClip = true;
+                    continue;
+                }
+                throw std::domain_error("bad soft clip");
+            }
+            countNotClip += 1;
+            if (op.isa(CIGAR_OP::padding))
+                continue;
+            if (op.isMatch())
+                op = op.changeOperation(CIGAR_OP::match);
+            if (v.empty() || !op.isSameOperation(v.back()))
+                v.emplace_back(op);
+            else
+                v.back().merge(op);
+        }
+        if (!parser.atEnd())
+            throw std::domain_error("invalid CIGAR");
+
+        // v contains only [MIDNB]; [SHP] were removed; [X=] were converted to M
+        // delete leading D/N; remove leading I/B and add to left soft clip
+        while (!v.empty() && !(v.front().isa(CIGAR_OP::match))) {
+            clip.first += v.front().qlength();
+            v.erase(v.begin());
+        }
+        // delete trailing D/N; remove trailing I/B and add to right soft clip
+        while (!v.empty() && !(v.front().isa(CIGAR_OP::match))) {
+            clip.second += v.front().qlength();
+            v.pop_back();
+        }
+        return { clip, v };
+    }
+    static ParseResult parseUnsafeUnchecked(std::string const &cigar) {
+        auto clip = std::pair<unsigned, unsigned>(0,0);
+        auto v = std::vector<CIGAR_OP>();
+        auto parser = CIGAR_OP::Parser(cigar.begin(), cigar.end());
+        
+        /*
+         * ([1-9][0-9]*S){0,1} #left soft clip
+         * ([1-9][0-9]*[MIDN])+ #regular CIGAR operations
+         * ([1-9][0-9]*S){0,1} #right soft clip
+         */
+        while (auto op = parser.next()) {
+            if (!op.isa(CIGAR_OP::softClip))
+                v.emplace_back(op);
+            else {
+                if (clip.first)
+                    clip.second = op.length();
+                else
+                    clip.first = op.length();
+            }
+        }
+        return { clip, v };
+    }
 public:
     CIGAR()
     : qlength(0)
@@ -162,118 +281,39 @@ public:
     , qfirst(0)
     , qclip(0)
     {}
-    explicit CIGAR(std::string const &str)
+    explicit CIGAR(std::string const &cigar, bool isClean = false)
     : qlength(0)
     , rlength(0)
     , qfirst(0)
     , qclip(0)
     {
-        unsigned i = 0;
-        auto isFirst = true;
-        auto p = CIGAR_OP::parse(str, i);
-        auto v = std::vector<CIGAR_OP>();
-        
-        /* ([1-9][0-9]*H){0,1} #left hard clip
-           ([1-9][0-9]*S){0,1} #left soft clip
-           ([1-9][0-9]*[MIDNPX=])+ #regular CIGAR operations
-           ([1-9][0-9]*S){0,1} #right soft clip
-           ([1-9][0-9]*H){0,1} #right hard clip
-         */
-        while (p.first != 0 && p.second != 0) {
-            auto const wasFirst = isFirst; isFirst = false;
-            if (p.second == 'H') {
-                if (wasFirst) goto NEXT;        ///< left hard clip; ignored
-                if (i == str.length()) break;   ///< right hard clip; ignored
-                goto INVALID;                   ///< invalid hard clip
+        try {
+            auto const parsed = (isClean ? CIGAR::parseUnsafeUnchecked : CIGAR::parse)(cigar);
+            if (!parsed.second.empty()) {
+                qfirst = parsed.first.first;
+                qclip = parsed.first.second;
+                assign(parsed.second.begin(), parsed.second.end());
             }
-            if (qclip != 0)
-                goto INVALID;                   ///< invalid, no operation other than 'H' should follow the right soft clip
-            if (p.second == 'P') goto NEXT;     ///< P is ignored
-            if (p.second == 'S') {
-                if (v.size() == 0) {
-                    if (qfirst != 0)
-                        goto INVALID;           ///< invalid soft clip
-                    qfirst = p.first;           ///< left soft clip
-                }
-                else
-                    qclip = p.first;            ///< right soft clip
-                
-                goto NEXT;
-            }
-            if (p.second == '=' || p.second == 'X') p.second = 'M'; ///< convert 'X' & '=' to 'M'
-            {
-                auto const elem = CIGAR_OP::compose(p);
-                if (elem.value == 0)
-                    goto INVALID;
-                
-                if (size() == 0 || (v.back().value & 0xF) != (elem.value & 0xF))
-                    v.push_back(elem);              ///< append to vector
-                else
-                    v.back().value += p.first << 4; ///< combine with last element
-            }
-        NEXT:
-            p = CIGAR_OP::parse(str, i);
         }
-        if (i != str.length()) goto INVALID;        ///< was the entire CIGAR string parsed?
-        
-        if (v.size() != 0) {
-            int first = 0;
-            
-            // convert+remove any leading 'I' into left soft clip
-            while (first != v.size() && v[first].opcode() == 'I') {
-                qfirst += v[first].length();
-                ++first;
-            }
+        catch (...) {}
 
-            // remove any trailing 'D' and convert+remove any trailing 'I' into right soft clip
-            int end = (int)v.size();
-            while (end - 1 >= first) {
-                auto const opcode = v[end - 1].opcode();
-                if (opcode != 'I' && opcode != 'D') break;
-                if (opcode == 'I')
-                    qclip += v[end - 1].length();
-                --end;
-            }
-            if (end > first)
-                assign(v.begin() + first, v.begin() + end);
-            else
-                goto INVALID;
+        qlength = qfirst + qclip;
+        for (auto && op : *this) {
+            rlength += op.rlength();
+            qlength += op.qlength();
         }
-        if (size() != 0 && front().opcode() == 'M' && back().opcode() == 'M') {
-            qlength = qfirst + qclip;
-            for (auto && op : *this) {
-                auto const length = op.length();
-                switch (op.value & 0xF) {
-                    case 0:
-                        rlength += length;
-                        // fallthrough;
-                    case 1:
-                    case 9:
-                        qlength += length;
-                        break;
-                    case 2:
-                    case 3:
-                        rlength += length;
-                    default:
-                        break;
-                }
-            }
-            return;
-        }
-    INVALID:
-        qclip = qfirst = qlength = rlength = 0;
-        clear();
     }
     operator std::string() const {
-        if (size() == 0) return "*";
-        auto rslt = std::string();
-        if (qfirst > 0)
-            rslt += CIGAR_OP::makeString(qfirst, 'S');
-        for (auto && i : *this) {
-            rslt += CIGAR_OP::makeString(i.length(), i.opcode());
+        auto rslt = std::string("*");
+        if (size() > 0) {
+            rslt.clear();
+            if (qfirst > 0)
+                CIGAR_OP(qfirst, CIGAR_OP::softClip).appendToString(rslt);
+            for (auto && i : *this)
+                i.appendToString(rslt);
+            if (qclip > 0)
+                CIGAR_OP(qclip, CIGAR_OP::softClip).appendToString(rslt);
         }
-        if (qclip > 0)
-            rslt += CIGAR_OP::makeString(qclip, 'S');
         return rslt;
     }
     CIGAR adjoint() const {

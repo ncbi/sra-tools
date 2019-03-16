7ec7f5fcd1c40390666901e59278e3570df82785
klymenko <klymenka@mail.nih.gov>
2019-03-16 05:32:47 -0400

    VDB-3723: mac fix

--- test/prefetch/check-size.pl
+++ test/prefetch/check-size.pl
@@ -3,9 +3,14 @@ use strict;
 die if ( $#ARGV < 0 );
 my $s = $ARGV [ 0 ];
 
-$_ = `wc -c tmp/sra/SRR053325.sra | cut -d" " -f1`;
+#print 'wc -c tmp/sra/SRR053325.sra | cut -d" " -f1' . "\n";
+$_ = `wc -c tmp/sra/SRR053325.sra`;
 chomp;
+@_ = split ( /\s+/ );
+die if ( $#_ <= 0 );
+$_ = $_ [ $#_ - 1 ];
 
+#print "size of tmp/sra/SRR053325.sra is $_\n";
 my $e = 0;
 #if ( $_ > 31681 )
 if ( $_ > $s )

4327c0c300b0192f3e1978c58597f3fb7e8cded2
durbrowk@ncbi.nlm.nih.gov <kmdurbrow@me.com>
2019-11-04 10:31:07 -0500

    Rng was excessive; changed to not use sprintf

--- tools/driver-tool/uuid.cpp
+++ tools/driver-tool/uuid.cpp
@@ -33,11 +33,39 @@
 #include <string>
 #include <random>
 #include <limits>
-#include <cstdio>
-#include <ctime>
-#include <cassert>
-
 #include "uuid.hpp"
+
+template <int COUNT, int BITS>
+class RandomValues
+{
+public:
+    using RandomValue = std::random_device::result_type;
+    using RandomValueType = std::numeric_limits<RandomValue>;
+    static_assert(RandomValueType::digits >= BITS && RandomValueType::is_signed == false, "!!!");
+    
+    static auto constexpr bits = RandomValueType::digits;
+    auto static constexpr count = (BITS * COUNT + bits - 1) / bits;
+
+    RandomValues() {
+        std::random_device devRandom;
+        for (auto i = 0; i < count; ++i)
+            value[i] = devRandom();
+    }
+    RandomValue next() {
+        if (b == 0) {
+            i += 1;
+            b = bits;
+        }
+        assert(i < count);
+        auto const result = value[i] & ((1u << BITS) - 1u);
+        value[i] >>= BITS;
+        b -= BITS;
+        return result;
+    }
+private:
+    unsigned i = 0, b = bits;
+    RandomValue value[count];
+};
 
 #ifndef TESTING
 static
@@ -47,20 +75,28 @@ static
 /// @param buffer filled in with new uuid
 void uuid_random(char buffer[37])
 {
-    std::random_device s; // seeder
-    auto const s1 = s();
-    auto const s2 = time(nullptr);
-    std::mt19937 gen1(s1 ^ decltype(s1)(s2 >> 32)), gen2(s1 ^ decltype(s1)(s2));
-    std::uniform_int_distribution<uint16_t> d16;
-    std::uniform_int_distribution<uint32_t> d32;
-
-    // xxxxxxxx-xxxx-4xxx-Nxxx-xxxxxxxxxxxx
-    //    08x    04x  04x  04x  04x   08x
-    sprintf(buffer, "%08x-%04x-%04x-%04x-%04x%08x"
-            , d32(gen1), d16(gen2)
-            , (d16(gen2) & 0x0FFF) | 0x4000 ///< type 4 (4 in high nibble)
-            , (d16(gen2) & 0x3FFF) | 0x8000 ///< version 1 (highest 2 bits set to 10)
-            , d16(gen2), d32(gen1));
+    auto rnd = RandomValues<31, 4>();
+    static char const *hexdigits = "0123456789abcdef";
+    
+    for (auto i = 0; i < 36; ++i) {
+        switch ("xxxxxxxx-xxxx-4xxx-Nxxx-xxxxxxxxxxxx"[i]) {
+                // x <= a random lowercase hex digit
+                // N <= a random character from [89ab]
+        case 'x':
+            buffer[i] = hexdigits[rnd.next()];
+            break;
+        case '-':
+            buffer[i] = '-';
+            break;
+        case '4':
+            buffer[i] = '4';
+            break;
+        case 'N':
+            buffer[i] = hexdigits[(rnd.next() & 0x03) | 0x08]; // take only the 2 low order bits and set the 2 high order bits to 10
+            break;
+        }
+    }
+    buffer[36] = '\0';
 }
 
 std::string uuid()

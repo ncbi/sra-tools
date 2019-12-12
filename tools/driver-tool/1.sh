dc9308dd55bb60c67bacb05689fe33398d1c5ed1
wraetz <raetzw@ncbi.nlm.nih.gov>
2019-12-12 13:29:26 -0500

    adjusted script to test prefetch

--- tools/driver-tool/1.sh
+++ tools/driver-tool/1.sh
@@ -5,4 +5,4 @@ PAR="A1 A2 A3"
 #CMN="--ngc some-ngc-file --kar some-kar-file --perm some-perm-file --location some-location"
 #FQ="-N 2 -X 5 --spot-groups 1,2,3 --read-filter split"
 #sratools $PAR --aligned-region chr1:20-30 --aligned-region chr2,xx -r chr3 -L 6 -h
-sratools $PAR --dryrun
+sratools -+VFS SRR4065643

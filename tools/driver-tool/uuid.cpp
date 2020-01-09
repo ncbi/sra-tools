66471318c6f1089e250ec6da66d892d111060bfd
durbrowk@ncbi.nlm.nih.gov <kmdurbrow@me.com>
2020-01-09 17:17:37 -0500

    Added tests

--- tools/driver-tool/uuid.cpp
+++ tools/driver-tool/uuid.cpp
@@ -68,13 +68,10 @@ private:
     RandomValue value[count];
 };
 
-#ifndef TESTING
-static
-#endif
 /// @brief generate a type 4 version 1 uuid
 ///
 /// @param buffer filled in with new uuid
-void uuid_random(char buffer[37])
+static void uuid_random(char buffer[37])
 {
     auto rnd = RandomValues<31, 4>();
     static char const *hexdigits = "0123456789abcdef";
@@ -106,3 +103,78 @@ std::string uuid()
     uuid_random(buffer);
     return std::string(buffer, buffer + 36);
 }
+
+#if DEBUG || _DEBUGGING
+static bool uuid_is_valid(std::string const &buffer)
+{
+    if (buffer.size() != 36) return false;
+
+    for (auto i = 0; i < 36; ++i) {
+        auto const ch = buffer[i];
+
+        // 0         1         2         3     36
+        // 012345678901234567890123456789012345
+        // xxxxxxxx-xxxx-4xxx-Nxxx-xxxxxxxxxxxx
+        switch (i) {
+        case 8:
+        case 13:
+        case 18:
+        case 23:
+            if (ch != '-') return false;
+            break;
+        case 14:
+            if (ch != '4') return false;
+            break;
+        case 19:
+            switch (ch) {
+            case '8':
+            case '9':
+            case 'a':
+            case 'b':
+                break;
+            default:
+                return false;
+            }
+            break;
+        default:
+            if (!isxdigit(ch)) return false;
+            break;
+        }
+    }
+    return true;
+}
+
+void uuid_test(void)
+{
+    assert(uuid_is_valid("a38fa5da-1456-498a-8ef5-1f39e35bfe57"));
+    assert(uuid_is_valid("a38fa5da-1456-498a-9ef5-1f39e35bfe57"));
+    assert(uuid_is_valid("a38fa5da-1456-498a-aef5-1f39e35bfe57"));
+    assert(uuid_is_valid("a38fa5da-1456-498a-bef5-1f39e35bfe57"));
+    assert(uuid_is_valid("a38fa5da-1456-098a-9ef5-1f39e35bfe57") == false); // invalid type
+    assert(uuid_is_valid("a38fa5da-1456-498a-0ef5-1f39e35bfe57") == false); // invalid subtype
+    assert(uuid_is_valid("a38fa5da11456-498a-9ef5-1f39e35bfe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38fa5da-14562498a-9ef5-1f39e35bfe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38fa5da-1456-498a39ef5-1f39e35bfe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38fa5da-1456-498a-9ef541f39e35bfe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38f-5da-1456-498a-9ef5-1f39e35bfe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38fa5da-14-6-498a-9ef5-1f39e35bfe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38fa5da-1456-49-a-9ef5-1f39e35bfe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38fa5da-1456-498a-9e-5-1f39e35bfe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38fa5da-1456-498a-9ef5-1f39e35-fe57") == false); // invalid seperator
+    assert(uuid_is_valid("a38fa5da-1456-498a-9ef5-1f39e35b") == false);     // too short
+    assert(uuid_is_valid("a38fa5da-1456-498a-9ef5-fe571f39e35bfe57") == false); // too long
+    assert(uuid_is_valid("a38fX5da-1456-498a-9ef5-1f39e35bfe57") == false); // invalid character
+    assert(uuid_is_valid("a38fa5da-14x6-498a-9ef5-1f39e35bfe57") == false); // invalid character
+    assert(uuid_is_valid("a38fa5da-1456-49za-9ef5-1f39e35bfe57") == false); // invalid character
+    assert(uuid_is_valid("a38fa5da-1456-498a-9ey5-1f39e35bfe57") == false); // invalid character
+    assert(uuid_is_valid("a38fa5da-1456-498a-9ef5-1fq9e35bfe57") == false); // invalid character
+    assert(uuid_is_valid("a38fa5da-1456-498a-9ef5-1f39e3Sbfe57") == false); // invalid character
+    assert(uuid_is_valid("a38fa5da-l33t-498a-9ef5-1f39e35bfe57") == false); // invalid character
+
+    for (auto i = 0; i < 100000; ++i) {
+        char buffer[37];
+        uuid_random(buffer);
+        assert(uuid_is_valid(buffer));
+    }
+}
+#endif

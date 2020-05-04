68426493ce82085e14cb4d25bffbf7ca49167955
durbrowk@ncbi.nlm.nih.gov <kmdurbrow@me.com>
2020-05-04 17:12:59 -0400

    removed

--- build/docker/generate-config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if [ ! -e user-settings.mkfg ] ; then
-    printf '/LIBS/GUID = "%.24s%.12s"\n' `cat build-id` `basename "$(awk -F: '$3~/^\/docker/{print $3;exit}' /proc/self/cgroup)"` > user-settings.mkfg
-fi

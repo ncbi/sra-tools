aa4e82c2f6dc442c5a068b7cd6d29acad5b739e1
Andrew Klymenko <klymenka@nih.gov>
2021-08-25 14:31:40 -0400

    VDB-4535: new full- and zero- quality preferences

--- tools/vdb-config/interactive2.cpp
+++ tools/vdb-config/interactive2.cpp
@@ -664,12 +664,12 @@ class vdbconf_view2 : public Dlg
                               model.is_site_enabled(), // model-connection
                               CB_COLOR_BG, CB_COLOR_FG, page_id );
             }
-            /*
+
             y += 2;
-            PopulateCheckbox( use_repo_rect( r, y ), resize, MAIN_FULL_QUALITY, "Use Full &Qualities",
+            PopulateCheckbox( use_repo_rect( r, y ), resize, MAIN_FULL_QUALITY, "Prefer Zero To Full-&Quality",
                               model.get_full_quality(), // model-connection
                               CB_COLOR_BG, CB_COLOR_FG, page_id );
-            */
+
             /* the GUID-label at the bottom */
             std::stringstream ss;
             ss << "GUID: " << model.get_guid();

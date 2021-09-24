5f8b54bdd4a9d7965b9920831bd98c8ed014c1aa
Alexander Ukrainchik <ukrainch@ncbi.nlm.nih.gov>
2021-09-24 00:21:23 -0400

    VDB-4527 fixed vdb-dump test, added prefetch, not ready yet

--- test/vdb-dump/vdb_dump.sh
+++ test/vdb-dump/vdb_dump.sh
@@ -1,60 +1,63 @@
 #!/bin/bash
 
-vdb-dump-makedb=$1
+vdb_dump_makedb=$1
 bin_dir=$2
-python_bin=$3
-cmake_source_dir=$4
 
+echo vdb-dump-makedb: ${vdb_dump_makedb}
 echo cmake_source_dir: ${cmake_source_dir}
 
 echo "testing vdb_dump"
 
 TEMPDIR=.
-CONFIGTOUSE ?= NCBI_SETTINGS
-TOP=${cmake_source_dir}
-echo TOP=${TOP}
 
-echo Testing ${bin_dir} CONFIGTOUSE=${CONFIGTOUSE}
-
-# TODO testing_returncode
+echo Testing vdb-dump from ${bin_dir}
 
 rm -rf actual
 mkdir -p actual
 
-if [ "${python_bin}" ]
-	then echo "python_bin is ${python_bin}"
-	${CONFIGTOUSE}=${TEMPDIR}/tmp.mkfg ${bin_dir}/vdb-dump SRR056386 -R 1 -C READ -f tab \
-	    >actual/1.0.stdout && diff expected/1.0.stdout actual/1.0.stdout
-	${CONFIGTOUSE}=/ ${bin_dir}/vdb-dump SRR056386 -R 1 -C READ -f tab -I \
-	    >actual/1.1.stdout && diff expected/1.1.stdout actual/1.1.stdout
-	# nested databases
-	${CONFIGTOUSE}=/ ${bin_dir}/vdb-dump -E data/NestedDatabase \
-	    >actual/2.0.stdout && diff expected/2.0.stdout actual/2.0.stdout
-	${CONFIGTOUSE}=/ ${bin_dir}/vdb-dump -T SUBDB_1.SUBSUBDB_1.TABLE1 \
-	       data/NestedDatabase >actual/2.1.stdout \
-	    && diff expected/2.1.stdout actual/2.1.stdout
-	${CONFIGTOUSE}=/ ${bin_dir}/vdb-dump -T SUBDB_1.SUBSUBDB_2.TABLE2 \
-	       data/NestedDatabase >actual/2.2.stdout \
-	    && diff expected/2.2.stdout actual/2.2.stdout
-	rm -rf actual
-	rm -rf data
-	${python_bin} ${TOP}/build/check-exit-code.py ${bin_dir}/vdb-dump
-	./test_buffer_insufficient.sh ${bin_dir}/vdb-dump VDB-3937.kar
-	echo "...all tests passed"
-else
-	echo "python_bin is empty"
-fi
+echo makedb:
+rm -rf data; mkdir -p data; ${vdb_dump_makedb}
 
-#output=$(NCBI_SETTINGS=${TEMPDIR}/tmp.mkfg \
-#	PATH="${bin_dir}:$PATH" \
-#	SRATOOLS_IMPERSONATE=fastq-dump \
-#	${bin_dir}/sratools --help | sed -e'/"fastq-dump" version/ s/version.*/version <deleted>/' >actual/help_fastq-dump.stdout ; \
-#	diff expected/help_fastq-dump.stdout actual/help_fastq-dump.stdout)
-
-
+echo output format
+output=$(NCBI_SETTINGS=/ ${bin_dir}/vdb-dump SRR056386 -R 1 -C READ -f tab > actual/1.0.stdout && diff expected/1.0.stdout actual/1.0.stdout)
 res=$?
 if [ "$res" != "0" ];
-	then echo "vdb_dump FAILED, res=$res output=$output" && exit 1;
+	then echo "vdb_dump 1.0 FAILED, res=$res output=$output" && exit 1;
 fi
+
+output=$(NCBI_SETTINGS=/ ${bin_dir}/vdb-dump SRR056386 -R 1 -C READ -f tab -I > actual/1.1.stdout && diff expected/1.1.stdout actual/1.1.stdout)
+res=$?
+if [ "$res" != "0" ];
+	then echo "vdb_dump 1.1 FAILED, res=$res output=$output" && exit 1;
+fi
+
+
+echo nested databases
+output=$(NCBI_SETTINGS=/ ${bin_dir}/vdb-dump -E data/NestedDatabase > actual/2.0.stdout && diff expected/2.0.stdout actual/2.0.stdout)
+res=$?
+if [ "$res" != "0" ];
+	then echo "vdb_dump 2.0 FAILED, res=$res output=$output" && exit 1;
+fi
+
+output=$(NCBI_SETTINGS=/ ${bin_dir}/vdb-dump -T SUBDB_1.SUBSUBDB_1.TABLE1 data/NestedDatabase > actual/2.1.stdout && diff expected/2.1.stdout actual/2.1.stdout)
+res=$?
+if [ "$res" != "0" ];
+	then echo "vdb_dump 2.1 FAILED, res=$res output=$output" && exit 1;
+fi
+
+output=$(NCBI_SETTINGS=/ ${bin_dir}/vdb-dump -T SUBDB_1.SUBSUBDB_2.TABLE2 data/NestedDatabase > actual/2.2.stdout && diff expected/2.2.stdout actual/2.2.stdout)
+res=$?
+if [ "$res" != "0" ];
+	then echo "vdb_dump 2.2 FAILED, res=$res output=$output" && exit 1;
+fi
+
+rm -rf actual
+rm -rf data
+./test_buffer_insufficient.sh ${bin_dir}/vdb-dump VDB-3937.kar
+res=$?
+if [ "$res" != "0" ];
+	then echo "test_buffer_insufficient FAILED, res=$res output=$output" && exit 1;
+fi
+echo "...all tests passed"
 
 echo vdb_dump is finished

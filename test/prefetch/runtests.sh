f675261387b0fa4e35c6ff177a604ac78fb73d85
aboshkin <boshkina@ncbi.nlm.nih.gov>
2021-10-06 14:16:43 -0400

    VDB-4527 debugging test/prefetch

--- test/prefetch/runtests.sh
+++ test/prefetch/runtests.sh
@@ -568,6 +568,7 @@ fi ;
 
 echo PREFETCH MULTIPLE ITEMS
 rm -fr `pwd`/SRR0* `pwd`/tmp/sra
+ls `pwd`/tmp
 export VDB_CONFIG=`pwd`/tmp; export NCBI_SETTINGS=/ ; \
   ${bin_dir}/prefetch SRR045450 ${SRAC} -+VFS #> /dev/null
 output=$(ls `pwd`/tmp/sra/SRR045450.sra `pwd`/tmp/sra/${SRAC}.sra)

950de9c99f0b6d31364852fff040f6d6d799b773
shk656461 <shkeda@ncbi.nlm.nih.gov>
2021-11-24 11:19:30 -0500

    Doxygen comments [JIRA:VDB-4618]

--- tools/sharq/fastq_read.hpp
+++ tools/sharq/fastq_read.hpp
@@ -1,16 +1,21 @@
 #ifndef __CFASTQREAD_HPP__
 #define __CFASTQREAD_HPP__
+/**
+ * @file fastq_read.hpp
+ * @brief FASTQ read
+ * 
+ */
 
 #include "fastq_error.hpp"
 #include <re2/re2.h>
 
 using namespace std;
 
-//  ============================================================================
 class CFastqRead
-//  ============================================================================
+/// FASTQ read
 {
 public:
+    /// ReadTypes 
     enum {
         SRA_READ_TYPE_TECHNICAL  = 0,
         SRA_READ_TYPE_BIOLOGICAL = 1
@@ -21,15 +26,8 @@ public:
 
     void Reset();
 
-//#if (__cplusplus >= 201703L) 
     void AddSequenceLine(const string_view& sequence);
     void AddQualityLine(const string_view& quality);
-//#else
-//    void AddSequenceLine(const string& sequence);
-//    void AddQualityLine(const string& quality);
-//#endif
-
-    //void Validate();
 
     bool Empty() const { return mSpot.empty(); }
 
@@ -41,28 +39,20 @@ public:
     const string& ReadNum() const { return mReadNum; }
     const string& SpotGroup() const { return mSpotGroup; }
     const uint8_t ReadFilter() const { return mReadFilter; }
-    const string& Sequence() const { return mSequence; }
-    const string& Quality() const { return mQuality; }
+    const string& Sequence() const { return mSequence; } ///< Sequence string
+    const string& Quality() const { return mQuality; } ///< Quality string
 
-    void GetQualScores(vector<uint8_t>& qual_score) const; ///< append quality scores to the input vector
+    /**
+     * Get quality scores as vector of uint8
+     * 
+     * @param[in,out] qual_score vector of scores 
+     * 
+    */
+    void GetQualScores(vector<uint8_t>& qual_score) const; ///< Get quality scores as vector of uint8
 
-    void SetType(char readType) {
-         switch (readType) {
-             case 'T':
-                mReadType = SRA_READ_TYPE_TECHNICAL;
-                break;
-            case 'B':
-                mReadType = SRA_READ_TYPE_BIOLOGICAL;
-                break;
-            case 'A':
-                 mReadType = Sequence().size() < 40 ? SRA_READ_TYPE_TECHNICAL: SRA_READ_TYPE_BIOLOGICAL;   
-                break;
-            default:
-                throw fastq_error(150, "Read {}: invalid readtType '{}'", mSpot, readType);
-         }
-    }
+    void SetType(char readType);
 
-    uint8_t Type() const { return mReadType;}
+    uint8_t Type() const { return mReadType;} ///< return ReadType 
 
     void SetLineNumber(size_t line_number) { mLineNumber = line_number;}
     void SetSpot(const string& spot) { mSpot = spot; }
@@ -74,31 +64,21 @@ public:
     void SetSuffix(const string& suffix) { mSuffix = suffix; }
     void SetSuffix(const re2::StringPiece& suffix) { suffix.CopyToString(&mSuffix); }
 
-    void SetSpotGroup(const string& spotGroup) { 
-        if (spotGroup == "0")
-            mSpotGroup.clear();
-        else 
-            mSpotGroup = spotGroup; 
-    }
-    void SetSpotGroup(const re2::StringPiece& spotGroup) { 
-        if (spotGroup == "0")
-            mSpotGroup.clear();
-        else 
-            spotGroup.CopyToString(&mSpotGroup); 
-    }
+    void SetSpotGroup(const string& spotGroup);
+    void SetSpotGroup(const re2::StringPiece& spotGroup);
 
     void SetReadFilter(uint8_t readFilter) { mReadFilter = readFilter; }
 
 private:
     friend class fastq_reader;
-    size_t mLineNumber{0};
-    string mSpot;
-    string mReadNum;
-    string mSpotGroup;
-    uint8_t mReadFilter{0};
-    uint8_t mReadType{0};
-    string mSuffix;
-    string mSequence;
+    size_t mLineNumber{0};        ///< Line number the read starts with
+    string mSpot;                 ///< Spot name
+    string mReadNum;              ///< optional read number
+    string mSpotGroup;            ///< spot group (barcode) 
+    uint8_t mReadFilter{0};       ///< read filter 0, 1
+    uint8_t mReadType{0};         ///< read type - SRA_READ_TYPE_TECHNICAL|SRA_READ_TYPE_BIOLOGICAL
+    string mSuffix;               ///< Illumina siffux
+    string mSequence;             ///< Sequence string
     string mQuality;              ///< Quality string as it comes from file adjusted to seq length
     vector<uint8_t> mQualScores;  ///< Numeric quality scores
 };
@@ -154,6 +134,40 @@ void CFastqRead::GetQualScores(vector<uint8_t>& qual_score) const
     } else {
         copy(mQualScores.begin(), mQualScores.end(), back_inserter(qual_score));
     }
+}
+
+void CFastqRead::SetType(char readType) 
+{
+    switch (readType) {
+        case 'T':
+        mReadType = SRA_READ_TYPE_TECHNICAL;
+        break;
+    case 'B':
+        mReadType = SRA_READ_TYPE_BIOLOGICAL;
+        break;
+    case 'A':
+            mReadType = Sequence().size() < 40 ? SRA_READ_TYPE_TECHNICAL: SRA_READ_TYPE_BIOLOGICAL;   
+        break;
+    default:
+        throw fastq_error(150, "Read {}: invalid readtType '{}'", mSpot, readType);
+    }
+}
+
+
+void CFastqRead::SetSpotGroup(const string& spotGroup) 
+{ 
+    if (spotGroup == "0")
+        mSpotGroup.clear();
+    else 
+        mSpotGroup = spotGroup; 
+}
+
+void CFastqRead::SetSpotGroup(const re2::StringPiece& spotGroup) 
+{ 
+    if (spotGroup == "0")
+        mSpotGroup.clear();
+    else 
+        spotGroup.CopyToString(&mSpotGroup); 
 }
 
 

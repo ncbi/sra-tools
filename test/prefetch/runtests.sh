a17c00ab404b126323b3d36fc77c483f4b648b9b
Andrew Klymenko <klymenka@nih.gov>
2021-12-13 14:38:09 -0500

    VDB-4691: restored vdbcache download test

--- test/prefetch/runtests.sh
+++ test/prefetch/runtests.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+# ===========================================================================
+#
+#                            PUBLIC DOMAIN NOTICE
+#               National Center for Biotechnology Information
+#
+#  This software/database is a "United States Government Work" under the
+#  terms of the United States Copyright Act.  It was written as part of
+#  the author's official duties as a United States Government employee and
+#  thus cannot be copyrighted.  This software/database is freely available
+#  to the public for use. The National Library of Medicine and the U.S.
+#  Government have not placed any restriction on its use or reproduction.
+#
+#  Although all reasonable efforts have been taken to ensure the accuracy
+#  and reliability of the software and data, the NLM and the U.S.
+#  Government do not and cannot warrant the performance or results that
+#  may be obtained by using this software or data. The NLM and the U.S.
+#  Government disclaim all warranties, express or implied, including
+#  warranties of performance, merchantability or fitness for any particular
+#  purpose.
+#
+#  Please cite the author in any work or product based on this material.
+#
+# ===========================================================================
 
 test_quality=$1
 bin_dir=$2
@@ -53,66 +76,6 @@ WGSF=${SRAF}:data/sracloud/traces/wgs03/WGS/AF/VF/AFVF01.1
 
 work_dir=$(pwd)
 echo WORK DIRECTORY: ${work_dir}
-
-# echo vdbcache:
-# echo vdbcache download
-
-# rm -frv SRR6667190*
-
-# echo "/LIBS/GUID = \"8test002-6ab7-41b2-bfd0-prefetchpref\"" > SRR6667190.kfg
-
-# # download sra and vdbcache
-# NCBI_SETTINGS=/ VDB_CONFIG=`pwd` \
-# ${bin_dir}/prefetch SRR6667190    > /dev/null
-# ls SRR6667190/SRR6667190.sra          > /dev/null
-# ls SRR6667190/SRR6667190.sra.vdbcache > /dev/null
-# res=$?
-# if [ "$res" != "0" ];
-	# then echo "prefetch vdbcache, res=$res output=$output" && exit 1;
-# fi
-# # TODO: remove test/prefetch/SRR6667190.kfg ?
-
-# # second run of prefetch finds local
-# NCBI_SETTINGS=/ VDB_CONFIG=`pwd` \
-# ${bin_dir}/prefetch SRR6667190    > /dev/null
-# res=$?
-# if [ "$res" != "0" ];
-	# then echo "prefetch vdbcache, res=$res output=$output" && exit 1;
-# fi
-
-# # download missed sra
-# rm SRR6667190/SRR6667190.sra
-# NCBI_SETTINGS=/ VDB_CONFIG=`pwd` \
-# ${bin_dir}/prefetch SRR6667190    > /dev/null
-# ls SRR6667190/SRR6667190.sra          > /dev/null
-# res=$?
-# if [ "$res" != "0" ];
-	# then echo "prefetch vdbcache, res=$res output=$output" && exit 1;
-# fi
-
-# # download missed vdbcache
-# rm SRR6667190/SRR6667190.sra.vdbcache
-# NCBI_SETTINGS=/ VDB_CONFIG=`pwd` \
-# ${bin_dir}/prefetch SRR6667190    > /dev/null
-# ls SRR6667190/SRR6667190.sra.vdbcache > /dev/null
-# res=$?
-# if [ "$res" != "0" ];
-	# then echo "prefetch vdbcache, res=$res output=$output" && exit 1;
-# fi
-
-# # prefetch works when AD is empty
-# rm SRR6667190/*
-# NCBI_SETTINGS=/ VDB_CONFIG=`pwd` \
-# ${bin_dir}/prefetch SRR6667190    > /dev/null
-# ls SRR6667190/SRR6667190.sra          > /dev/null
-# ls SRR6667190/SRR6667190.sra.vdbcache > /dev/null
-# res=$?
-# if [ "$res" != "0" ];
-	# then echo "prefetch vdbcache, res=$res output=$output" && exit 1;
-# fi
-
-# # ls SRR6667190
-# rm -r SRR6667190*
 
 # echo ncbi1GB:
 # mkdir -p tmp

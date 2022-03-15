6df661cfeed1d39dd7bbb7183fff3abbd98fc594
Kenneth Durbrow <durbrow@users.noreply.github.com>
2022-03-15 16:41:44 -0400

    Fix missing settings value for cloud test.

--- test/driver-tool/testing.sh
+++ test/driver-tool/testing.sh
@@ -1,23 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
-bin_dir=$1
+[ $# -gt 0 ] || { echo "Usage: $0 <binaries directory>"; exit 1; }
 
-echo "testing expected output for testing modes with bad inputs testing"
+SRATOOLS="$1/sratools"
+[ -x "${SRATOOLS}" ] || { echo "no sratools in '$1'"; exit 1; }
 
-TEMPDIR=.
+SETTINGS=$(cat << 'EOF'
+/LIBS/GUID = "c1d99592-6ab7-41b2-bfd0-8aeba5ef8498"
+/libs/cloud/report_instance_identity = "true"
+EOF
+)
+
+SETTINGS_FILE=$(mktemp "${TMPDIR:-/tmp}/$(basename $0).XXXXXX.mkfg")
+printf '%s' "${SETTINGS}" > "${SETTINGS_FILE}" || exit 1
 
 mkdir -p actual
 
-output=$(NCBI_SETTINGS=${TEMPDIR}/tmp.mkfg \
-	PATH="${bin_dir}:$PATH" \
-	SRATOOLS_TESTING=2 \
-	SRATOOLS_IMPERSONATE=prefetch \
-	${bin_dir}/sratools --perm foo.jwt --cart foo.cart --ngc foo.ngc DRX000001 2>actual/testing.stderr ; \
-	diff expected/testing.stderr actual/testing.stderr || diff expected/testing-cloudy.stderr actual/testing.stderr)
+echo 'Checking that test modes allow bad inputs.'
 
+env NCBI_SETTINGS="${SETTINGS_FILE}" \
+    SRATOOLS_TESTING=2 \
+    SRATOOLS_IMPERSONATE=prefetch \
+    "${SRATOOLS}" --perm foo.jwt --cart foo.cart --ngc foo.ngc DRX000001 2>actual/testing.stderr
+    
+diff -q expected/testing.stderr actual/testing.stderr >/dev/null || diff -q expected/testing-cloudy.stderr actual/testing.stderr >/dev/null
 res=$?
-if [ "$res" != "0" ];
-	then echo "Driver tool test testing FAILED, res=$res output=$output" && exit 1;
-fi
 
-echo Driver tool test testing is finished
+rm -rf "${NCBI_SETTINGS}"
+[ ${res} -eq 0 ] && { echo "Driver tool test 'testing' finished."; exit 0; }
+
+echo "Driver tool test 'testing' FAILED!"
+diff expected/testing.stderr actual/testing.stderr || diff expected/testing-cloudy.stderr actual/testing.stderr 
+exit 1

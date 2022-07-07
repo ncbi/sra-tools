e9322ad82abeed82df52b9963c8a3d099b330255
Kenneth Durbrow <durbrow@users.noreply.github.com>
2022-07-07 10:27:05 -0400

    Script to generate args info file

---
+++ tools/driver-tool/generate-args-info.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+[ ${#} -gt 0 ] || { echo "Usage: ${0} <sratools binaries directory>"; exit 1; }
+[ -d ${1} ] || { echo "${1} no such directory"; exit 1; }
+
+BINDIR=$(cd "${1}"; echo $PWD) || exit 1
+[ -d "${BINDIR}" ] || { echo "${BINDIR} no such directory" >&2 ; exit 1; }
+
+CONV_OUT="${PWD}/tool-arguments.h"
+{
+    echo     "/* Tool arguments definitions"
+    date -u +" * auto-generated at %Y-%m-%dT%H:%M:%SZ"
+    echo     " */"
+    echo     ""
+} > "${CONV_OUT}"
+(
+    for exe in 'vdb-dump' 'fasterq-dump' 'sam-dump' 'sra-pileup'
+    do
+        EXEPATH="${BINDIR}/${exe}"
+        [ -x "${EXEPATH}" ] || { echo "${EXEPATH} no such executable" >&2 ; exit 1; }
+        echo "${EXEPATH}"
+        SRATOOLS_DUMP_OPTIONS="${CONV_OUT}" "${EXEPATH}" || exit $?
+    done
+)
+exit $?

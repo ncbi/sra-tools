32375a234d31d73abfeb1a95c5009146aff4ac1d
mystcat <fkarmanov@gmail.com>
2022-07-08 11:40:52 -0400

    .pbi file format doesn't need unpacking

--- tools/copycat/ccfileformat.c
+++ tools/copycat/ccfileformat.c
@@ -105,6 +105,7 @@ static const char exttable [] =
     "tar\tTapeArchive\n"
     "xml\tExtensibleMarkupLanguage\n"
     "h5\tHD5\n"
+    "pbi\tPacBioBAMIndex\n"
 };
 
 static const char classtable [] = 
@@ -127,6 +128,7 @@ static const char formattable [] =
     "StandardFlowgramFormat\tRead\n"
     "TapeArchive\tArchive\n"
     "HD5\tArchive\n"
+    "PacBioBAMIndex\tRead\n"
 };
 
 static const char magicpath [] = "/usr/share/misc/magic";
@@ -306,9 +308,17 @@ rc_t CCFileFormatGetType (const CCFileFormat * self, const KFile * file,
                                      (klogWarn, "File '$(path)' is in unzupported winzip/pkzip format",
                                       "path=%s", path));
                         }
-                        else if (!strcmp("BinaryAlignmentMap", etypebuf) && !strcmp ("GnuZip", mtypebuf))
+                        else if (strcmp("BinaryAlignmentMap", etypebuf) == 0 && strcmp ("GnuZip", mtypebuf) == 0)
                         {
                             /* bam files have gnuzip magic, we need to treat them as data files ***/
+                            strcpy (mclassbuf, eclassbuf );
+                            strcpy (mtypebuf, etypebuf);
+                            mtype = etype;
+                            mclass = eclass;
+                        }
+                        else if (strcmp("PacBioBAMIndex", etypebuf) == 0 && strcmp("GnuZip", mtypebuf) == 0)
+                        {
+                            /* pbi files have gnuzip magic, we need to treat them as data files ***/
                             strcpy (mclassbuf, eclassbuf );
                             strcpy (mtypebuf, etypebuf);
                             mtype = etype;

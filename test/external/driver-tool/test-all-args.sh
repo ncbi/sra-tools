cd51eb3cedbde6633cde1167260ea1602429804c
Kenneth Durbrow <kmdurbrow@me.com>
2022-11-01 14:01:36 -0400

    More explicit and more verbose before exit

--- test/external/driver-tool/test-all-args.sh
+++ test/external/driver-tool/test-all-args.sh
@@ -8,14 +8,31 @@ ALL_INSTALLED='YES'
 ANY_INSTALLED='NO'
 for exe in 'vdb-dump' 'fasterq-dump' 'sam-dump' 'sra-pileup'
 do
-    [ -x "${BINDIR}/${exe}-orig.${VERSION}" ] || ALL_INSTALLED='NO'
-    [ -x "${BINDIR}/${exe}-orig.${VERSION}" ] && ANY_INSTALLED='YES'
+    if [ -x "${BINDIR}/${exe}-orig.${VERSION}" ]
+    then
+        ANY_INSTALLED='YES'
+    else
+        ALL_INSTALLED='NO'
+    fi
 done
 
-[ "${ALL_INSTALLED}" = "${ANY_INSTALLED}" ] && { echo 'some tools are installed and some are not!?' >&2 ; exit 1; }
+if [ "${ALL_INSTALLED}" != "${ANY_INSTALLED}" ]
+then
+    echo 'some tools are installed and some are not!?' >&2
+    for exe in 'vdb-dump' 'fasterq-dump' 'sam-dump' 'sra-pileup'
+    do
+        if [ -x "${BINDIR}/${exe}-orig.${VERSION}" ]
+        then
+            echo "found installed ${exe}"
+        else
+            echo "did not find installed ${exe}"
+        fi
+    done
+    exit 1
+fi
 INSTALLED=${ALL_INSTALLED}
 
 TEST_SOURCE="${PWD}"
 "${BINDIR}/sratools.${VERSION}" print-args-json \
 | python3 "${TEST_SOURCE}/test-tool-args.py" --installed "${INSTALLED}" --path "${BINDIR}" --use-version "${VERSION}" "${@}" \
-| sh
+# | sh

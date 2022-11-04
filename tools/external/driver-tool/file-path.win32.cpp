e22d79b573ac4ae96154094cbdcbcdcca501b99c
Kenneth Durbrow <kmdurbrow@me.com>
2022-11-04 10:21:53 -0400

    Revert "Should not have POSIX path seperators"
    This reverts commit 553bbb661f7435b84320d7dae07e15df488cd1f9.

--- tools/external/driver-tool/file-path.win32.cpp
+++ tools/external/driver-tool/file-path.win32.cpp
@@ -433,7 +433,7 @@ FilePath::operator std::string() const
 #if USE_WIDE_API
 FilePath::operator std::wstring() const
 {
-    return path.empty() ? path : canonicalPath(path);
+    return path.empty() ? path : std::wstring(canonicalPathPOSIX(path).get());
 }
 
 size_t FilePath::size() const
@@ -441,6 +441,7 @@ size_t FilePath::size() const
     auto const &asString = this->operator std::wstring();
     return asString.size();
 }
+#else
 #endif
 
 static NativeString trimPath(NativeString const &path, bool canTrim = true)

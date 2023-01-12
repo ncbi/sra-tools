5b74ddd8b00abc8d72b9578f199612967a9c76c9
Karmanov, Fedor <fedor.karmanov@nih.gov>
2023-01-12 15:01:24 -0500

    SRA-12087: FASTQ support - automated test added for fastq parser

--- test/internal/copycat/test-fastq.sh
+++ test/internal/copycat/test-fastq.sh
@@ -13,8 +13,8 @@ for f in input/*.fq; do
   output=$(${bin_dir}/${tool_binary} $f /dev/null | sed -rn 's/^(.*) mtime="([[:print:]]{20,20})"(.*)/\1\3/p' > actual/${tn}.out && diff -q input/${tn}.out actual/${tn}.out)
   res=$?
   if [ "$res" != "0" ];
-    then echo "${tool_binary} copy failed, res=${res} output=${output}" && exit 1;
+    then echo "${tool_binary} fastq parser failed, res=${res} output=${output}" && exit 1;
   fi
 done
 
-echo "${tool_binary} copy succeeded"
+echo "${tool_binary} fastq parser succeeded"

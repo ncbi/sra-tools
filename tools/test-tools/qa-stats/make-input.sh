a7cc8a0a21fb59902c3a5509225ef95a4170fa1b
durbrowk@ncbi.nlm.nih.gov <kmdurbrow@me.com>
2023-01-13 01:10:40 -0500

    usage and check for alignment ids

--- tools/test-tools/qa-stats/make-input.sh
+++ tools/test-tools/qa-stats/make-input.sh
@@ -2,13 +2,17 @@
 
 VDBDUMP=$(which vdb-dump) || exit $?
 
+[ "${1}" == "" ] && { echo "usage: ${0} <path or accession>"; exit 1; }
+
 ${VDBDUMP} -E ${1} | grep -q -e "tbl\s*:\s*PRIMARY_ALIGNMENT" \
 && ${VDBDUMP} -f tab ${1} -T PRIMARY_ALIGNMENT -C "READ,REF_NAME,REF_POS,REF_ORIENTATION,CIGAR_SHORT,SEQ_SPOT_GROUP"
 
 ${VDBDUMP} -E ${1} | grep -q -e "tbl\s*:\s*SECONDARY_ALIGNMENT" \
 && ${VDBDUMP} -f tab ${1} -T SECONDARY_ALIGNMENT -C "READ,REF_NAME,REF_POS,REF_ORIENTATION,CIGAR_SHORT,SEQ_SPOT_GROUP"
 
-${VDBDUMP} -f tab ${1} -C "CMP_READ,READ_LEN,READ_START,READ_TYPE,PRIMARY_ALIGNMENT_IDS,SPOT_GROUP" \
+${VDBDUMP} -o ${1} | grep -q -e "^CMP_READ" \
+&& ${VDBDUMP} -o ${1} | grep -q -e "^PRIMARY_ALIGNMENT_IDS" \
+&& ${VDBDUMP} -f tab ${1} -C "CMP_READ,READ_LEN,READ_START,READ_TYPE,PRIMARY_ALIGNMENT_IDS,SPOT_GROUP" \
 && exit 0
 
 ${VDBDUMP} -f tab ${1} -C "READ,READ_LEN,READ_START,READ_TYPE,SPOT_GROUP"

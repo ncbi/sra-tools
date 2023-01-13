f375387943dc0509d6b72b23d5bc4c01aa260a98
durbrowk@ncbi.nlm.nih.gov <kmdurbrow@me.com>
2023-01-13 12:40:41 -0500

    Add reordering tool

---
+++ tools/test-tools/qa-stats/make-test-input.pl
@@ -0,0 +1,62 @@
+#!perl -w
+
+use strict;
+use warnings;
+
+my $shuffle = 1;
+my $rev = 1;
+my $cmpl = 1;
+
+for (@ARGV) {
+    if (/no-shuffle/) {
+        $shuffle = 0;
+        next;
+    }
+    if (/no-rev/) {
+        $rev = 0;
+        next;
+    }
+    if (/no-cmpl/) {
+        $cmpl = 0;
+        next;
+    }
+    if (/help/) {
+        print "$0 [no-shuffle] [no-rev] [no-cmpl] [help]\n";
+        exit 0;
+    }
+    print "usage: $0 [no-shuffle] [no-rev] [no-cmpl] [help]\n";
+    exit 1;
+}
+
+my @output;
+
+while (defined($_ = <>)) {
+    my $m = 0;
+    
+    if ($rev && rand() < 0.5) {
+        $m |= 1
+    }
+    if ($cmpl && rand() < 0.5) {
+        $m |= 2
+    }
+    
+    if ($m) {
+        my @F = \split /\t/;
+        my $r = $F[0];
+        if ($m & 1) {
+            $$r = reverse $$r;
+        }
+        if ($m & 2) {
+            $$r =~ tr/ACGT/TGCA/;
+        }
+    }
+    if ($shuffle && @output) {
+        my $i = int(rand(scalar(@output) + 1));
+        if ($i < scalar(@output)) {
+            ($_, $output[$i]) = ($output[$i], $_)
+        }
+    }
+    push @output, $_;
+}
+
+print @output;

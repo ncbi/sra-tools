6055f5057cb20a74e55f4d5979aa843c468b16d9
wraetz <raetzw@nih.gov>
2023-05-01 11:05:00 -0400

    simple check for return-code on failure

---
+++ test/internal/vdb-copy/return_code_on_error.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VDB_COPY="vdb-copy"
+if [ ! -x $VDB_COPY ]; then
+    TOOL_PATH="$HOME/ncbi-outdir/sra-tools/linux/gcc/x86_64/dbg/bin"
+    VDB_COPY="${TOOL_PATH}/$VDB_COPY"
+fi
+
+if [ ! -x $VDB_COPY ]; then
+    echo "cannot find executable for vdb-copy"
+    exit 3
+fi
+
+if $VDB_COPY NOT_EXISTING_DIR LOCAL_COPY ; then
+    echo "ERROR: this should not succeed!"
+    exit 3
+else
+    echo "SUCCESS: vdb-copy returned $? if ask to copy a none-existing object."
+fi

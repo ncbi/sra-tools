2e8d50f021ca0ff9824dc9f2b02a13ce6c43b5d7
wraetz <raetzw@nih.gov>
2023-05-01 09:11:08 -0400

    helper script to test vdb-copy

---
+++ test/internal/vdb-copy/copy_and_compare.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+TOOL_PATH="$HOME/ncbi-outdir/sra-tools/linux/gcc/x86_64/dbg/bin"
+
+VDB_COPY="vdb-copy"
+if [ ! -x $VDB_COPY ]; then
+    VDB_COPY="${TOOL_PATH}/$VDB_COPY"
+fi
+
+if [ ! -x $VDB_COPY ]; then
+    echo "cannot find executable for vdb-copy"
+    exit 3
+fi
+
+ACC="$1"
+ACC_COPY="the_copy"
+
+if [ -d $ACC_COPY ]; then
+    rm -rf $ACC_COPY
+fi
+
+$VDB_COPY $ACC $ACC_COPY -p
+
+VDB_DIFF="vdb-diff"
+if [ ! -x $VDB_DIFF ]; then
+    VDB_DIFF="${TOOL_PATH}/$VDB_DIFF"
+fi
+
+if [ ! -x $VDB_DIFF ]; then
+    echo "cannot find executable for vdb-diff"
+    exit 3
+fi
+
+$VDB_DIFF $ACC $ACC_COPY -pc
+
+if [ -d $ACC_COPY ]; then
+    rm -rf $ACC_COPY
+fi

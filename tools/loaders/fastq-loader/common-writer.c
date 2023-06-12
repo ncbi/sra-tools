71c57e5df33071b3e49151aaf2c7d063bb88e81d
Alexander Ukrainchik <ukrainch@ncbi.nlm.nih.gov>
2023-06-12 13:06:03 -0400

    VDB-4667 fixing Test_FastqLoader_* segfaults on clang

--- tools/loaders/fastq-loader/common-writer.c
+++ tools/loaders/fastq-loader/common-writer.c
@@ -478,7 +478,7 @@ static void readRejected(Rejected const *const reject, struct ReadResult *const 
 
     if (rc == 0) {
         rslt->type = rr_rejected;
-        rslt->u.reject.message = strdup(message);
+        rslt->u.reject.message = string_dup_measure(message, NULL);
         rslt->u.reject.line = line;
         rslt->u.reject.column = col;
         rslt->u.reject.fatal = fatal;

5664466794b2aa4fcfe01166079eba5d42563a27
kdurbrow <durbrowk@ncbi.nlm.nih.gov>
2023-06-21 16:19:54 -0400

    Added test

---
+++ test/loaders/bam-load/test-small-unsorted.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+BINDIR=${1:-BINDIR}
+BAMLOAD=$(readlink -f ${BINDIR}/bam-load)
+VDBDUMP=$(readlink -f ${BINDIR}/vdb-dump)
+
+${BAMLOAD} small-unsorted.sam \
+    --log-level err \
+    --ref-file small-unsorted.fasta \
+    --output small-unsorted.out || exit 1
+
+${VDBDUMP} small-unsorted.out \
+    --table PRIMARY_ALIGNMENT \
+    --columns SPOT_COUNT \
+    --rows 1 \
+    --format tab > expected.small-unsorted
+    
+${VDBDUMP} small-unsorted.out \
+    --table REFERENCE \
+    --columns PRIMARY_ALIGNMENT_IDS \
+    --numelem \
+    --rows 1 \
+    --format tab > actual.small-unsorted
+
+diff -q expected.small-unsorted actual.small-unsorted >/dev/null
+DIFF=${?}
+rm -rf expected.small-unsorted actual.small-unsorted small-unsorted.out
+exit ${DIFF}
+

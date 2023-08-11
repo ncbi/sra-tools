50f8b65d775f183ea3a179ba5650789a44697a25
Jason W. Bacon <bacon4000@gmail.com>
2023-08-11 09:29:03 -0500

    Import patches from FreeBSD port

--- tools/external/driver-tool/file-path.posix.cpp
+++ tools/external/driver-tool/file-path.posix.cpp
@@ -193,7 +193,16 @@ static char const *find_executable_path(char const *const *const extra, char con
 FilePath FilePath::fullPathToExecutable(char const *const *const argv, char const *const *const envp, char const *const *const extra)
 {
     char const *path;
-#if LINUX
+// Assume sra-tools is installed by FreeBSD ports.  FreeBSD users
+// don't often install software by other means.  Default prefix
+// is /usr/local, but this can be overridden by the user building
+// the port.  The port can replace /usr/local using sed if that's the
+// case.
+#if BSD
+    char full_path[PATH_MAX];
+    snprintf(full_path, PATH_MAX, "/usr/local/bin/%s", argv[0]);
+    path = full_path;
+#elif LINUX
     path = "/proc/self/exe";
 #elif MAC
     path = find_executable_path(extra, argv[0]);
--- tools/external/driver-tool/sratools.cpp
+++ tools/external/driver-tool/sratools.cpp
@@ -420,6 +420,7 @@ static int main(CommandLine const &argv)
     enableLogging(argv.toolName.c_str());
 #endif
     LOG(7) << "executable path: " << (std::string)argv.fullPathToExe << std::endl;
+    // std::cerr << "executable path: " << (std::string)argv.fullPathToExe << std::endl;
 
     config = new Config();
     struct Defer { ~Defer() { delete config; config = nullptr; } } freeConfig;
@@ -576,6 +577,14 @@ static int main(CommandLine const &argv)
 }
 
 } // namespace sratools
+
+#if BSD
+int main(int argc, char *argv[], char *envp[])
+{
+    auto const invocation = CommandLine(argc, argv, envp, nullptr);
+    return sratools::main(invocation);
+}
+#endif
 
 #if MAC
 int main(int argc, char *argv[], char *envp[], char *apple[])

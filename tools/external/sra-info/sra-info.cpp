c164b7e075890e2aa10ceebccb9f7a0692be0a5f
Kenneth Durbrow <kmdurbrow@me.com>
2024-09-25 15:27:11 -0400

    Simplified

--- tools/external/sra-info/sra-info.cpp
+++ tools/external/sra-info/sra-info.cpp
@@ -1,28 +1,28 @@
-/*==============================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-*/
+/* ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ */
 
 #include "sra-info.hpp"
 
@@ -71,32 +71,25 @@ SraInfo::SetAccession( const std::string& p_accession )
     releaseDataObject();
     m_accession = p_accession;
     m_type = m_mgr.pathType(m_accession);
-    m_schemaType = "";
-    if (m_type == VDB::Manager::ptTable) {
-        m_u.tbl = new VDB::Table { m_mgr.openTable(m_accession) };
-        try {
-            m_schemaType = m_u.tbl->metadata().childNode("schema").attributeValue("name");
-        }
-        catch (...) {
-            m_type = VDB::Manager::ptPrereleaseTable;
-            delete m_u.tbl;
-            m_u.tbl = nullptr;
+    try {
+        switch (m_type) {
+        case VDB::Manager::ptDatabase:
+            m_u.db = new VDB::Database{ m_mgr.openDatabase(m_accession) };
+            return;
+        case VDB::Manager::ptTable:
+            m_u.tbl = new VDB::Table { m_mgr.openTable(m_accession) };
+            return;
+        case VDB::Manager::ptPrereleaseTable :
+            break;
+        default:
+            throw VDB::Error( (m_accession + ": unknown data type").c_str() );
         }
     }
-    switch (m_type) {
-    case VDB::Manager::ptDatabase:
-        assert(m_u.db == nullptr);
-        m_u.db = new VDB::Database{ m_mgr.openDatabase(m_accession) };
-        m_schemaType = m_u.db->metadata().childNode("schema").attributeValue("name");
-        break;
-    case VDB::Manager::ptTable:
-        assert(m_u.tbl != nullptr);
-        break;
-    case VDB::Manager::ptPrereleaseTable :
-        throw VDB::Error( (m_accession + ": obsolete data type").c_str(), __FILE__, __LINE__);
-    default:
-        throw VDB::Error( (m_accession + ": unknown data type").c_str(), __FILE__, __LINE__);
+    catch (VDB::Error const &err) {
+        if ((int)err.getRc() != 1434782232)
+            throw;
     }
+    throw VDB::Error( (m_accession + ": obsolete data type").c_str() );
 }
 
 bool SraInfo::isDatabase() const {

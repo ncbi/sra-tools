91bb83ca2bcef6407be6396a1e8f09714130d90f
Andrew Klymenko <klymenka@ncbi.nlm.nih.gov>
2024-12-03 13:49:27 -0500

    Revert "Revert "VDB-5784: debugging...""
    This reverts commit b80d507618a81923c44b397ebeb3f688e69038e0.

--- test/external/align-info/test-refseq-404.sh
+++ test/external/align-info/test-refseq-404.sh
@@ -2,15 +2,16 @@
 
 bin_dir=$1
 
-#perl -e 'print "NC_000005.8=";print $ENV{"NC_000005.8"};print"\n"'
-#echo NCBI_SETTINGS=$NCBI_SETTINGS
-#echo NCBI_VDB_NO_ETC_NCBI_KFG=$NCBI_VDB_NO_ETC_NCBI_KFG
+perl -e 'print "NC_000005.8=";print $ENV{"NC_000005.8"};print"\n"'
+echo NCBI_SETTINGS=$NCBI_SETTINGS
+echo NCBI_VDB_NO_ETC_NCBI_KFG=$NCBI_VDB_NO_ETC_NCBI_KFG
 
-#$bin_dir/vdb-config -on
-#$bin_dir/srapath NC_000005.8
+$bin_dir/vdb-config -on
+$bin_dir/srapath NC_000005.8
 
 # Verify that phid and error message are printed
 # when SDL fails to resolve RefSeq
+$bin_dir/align-info SRR619505
 $bin_dir/align-info SRR619505 2>&1 | grep -q "ncbi_phid='aHh'"
 res=$?
 if [ "$res" != "0" ] ; then

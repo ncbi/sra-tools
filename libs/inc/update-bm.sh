fa47c7c02ad64f5a298a5dc19217630d21022585
Kenneth Durbrow <kmdurbrow@me.com>
2025-04-04 16:57:18 -0400

    Script to update BitMagic

---
+++ libs/inc/update-bm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+TAG="${TAG:-v8.0.1}"
+SRC=$(pwd)
+SCRATCH=$(mktemp --directory) || exit 1
+# echo "${SCRATCH}"
+
+SAVE=$(trap)
+# echo "${SAVE}"
+trap "cd ${SRC}; rm -rf ${SCRATCH} ${SRC}/bm.moved-aside; trap '${SAVE}' INT EXIT TERM" INT EXIT TERM
+
+cd ${SCRATCH}
+git clone --branch "${TAG}" --depth 1 https://github.com/tlk00/BitMagic.git 2>/dev/null || exit 1
+mv ${SRC}/bm ${SRC}/bm.moved-aside || exit 1
+mv BitMagic/src ${SRC}/bm

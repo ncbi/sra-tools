943d78774456e0593de996e9e7218e67d2887524
Kenneth Durbrow <kmdurbrow@me.com>
2025-06-17 14:26:50 -0400

    Fix leak

--- tools/internal/rd-filter-redact/read-filter-redact.cpp
+++ tools/internal/rd-filter-redact/read-filter-redact.cpp
@@ -789,6 +789,7 @@ static rc_t enter_fingerprint( KMDataNode *node, Fingerprint & fp )
         json << '}';
         rc = KMDataNodeWrite ( fp_node, strm.str().data(), strm.str().size() );
     }
+    KMDataNodeRelease(fp_node);
     return rc;
 }
 

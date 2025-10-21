407d2ec1f96c19021eb84bee677e37965974a713
Andrew Klymenko <klymenka@ncbi.nlm.nih.gov>
2025-10-21 13:21:14 -0400

    VDB-6167: debugging...

--- test/tarballs/grep.py
+++ test/tarballs/grep.py
@@ -1,7 +1,9 @@
 import re
 import sys
 
-#print(sys.argv[1])
+num_arguments = len(sys.argv)
+print(f"grep.py: Total number of command-line arguments: {num_arguments}")
+print("grep.py: sys.argv[1]='" + sys.argv[1] + "'")
 found = 0
 version = ""
 

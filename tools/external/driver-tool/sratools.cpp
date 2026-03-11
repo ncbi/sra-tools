90783b530d7b9e753e17ba093fe783a28a81aaf0
aboshkin <boshkina@ncbi.nlm.nih.gov>
2026-03-11 13:31:21 -0400

    VDB-5034 fixed for Windows

--- tools/external/driver-tool/sratools.cpp
+++ tools/external/driver-tool/sratools.cpp
@@ -531,6 +531,7 @@ static int main(CommandLine const &argv)
             auto success = false;
 
             for (auto src : sources) {
+
                 if (verbosity > 0 && src.haveQualityType()) {
                     auto const name = src.haveFullQuality() ? fullQualityName : zeroQualityName;
                     auto const desc = src.haveFullQuality() ? fullQualityDesc : zeroQualityDesc;
@@ -543,16 +544,22 @@ static int main(CommandLine const &argv)
                     LOG(2) << "Processed " << acc << " with data from " << src.service << std::endl;
                     break;
                 }
-                if (result.didExit()) {
+                if (result.didExit()) { // this is how it always ends in Windows
                     auto const exit_code = result.exitCode();
                     if (exit_code == EX_TEMPFAIL) {
                         LOG(1) << "Failed to get data for " << acc << " from " << src.service << std::endl;
                         continue;
                     }
-                    std::cerr << argv.toolName << " quit with error code " << exit_code << std::endl;
+                     std::cerr << argv.toolName << " quit with error code "
+                        << "0x"
+                        << std::hex          // switch to hexadecimal format
+                        << std::uppercase     // use uppercase letters A-F
+                        << exit_code
+                        << std::dec          // switch back to decimal for future outputs
+                        << std::endl;
                     exit(exit_code);
                 }
-                // was killed or something
+                // was killed or something (POSIX)
                 exit(result.exitCode());
             }
             if (!success) {

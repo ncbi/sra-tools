5c2c634880d17bd79eeab738671cee315c78b52f
aboshkin <boshkina@ncbi.nlm.nih.gov>
2026-03-27 12:46:03 -0400

    VDB-4307 updated meta-repair

--- tools/internal/meta-repair/meta-repair.c
+++ tools/internal/meta-repair/meta-repair.c
@@ -1275,8 +1275,7 @@ rc_t CC Usage (const Args *args){
 
 MAIN_DECL( argc, argv )
 {
-    if ( VdbInitialize( argc, argv, 0 ) )
-        return VDB_INIT_FAILED;
+    VDB_INITIALIZE(argc, argv, VDB_INIT_FAILED);
 
     rc_t rc = 0, r2 = 0;
     Args *args=NULL;

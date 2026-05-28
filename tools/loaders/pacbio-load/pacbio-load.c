23b19635777de1e95f898dfdbad8ae841f4ecd21
Rahim Rajwani <rajwanir2@gmail.com>
2026-05-28 16:54:07 -0400

    Resolves VDB-6353. Add a summary section for pacbio-load in CLI help message.

--- tools/loaders/pacbio-load/pacbio-load.c
+++ tools/loaders/pacbio-load/pacbio-load.c
@@ -71,6 +71,7 @@ const char UsageDefaultName[] = "pacbio-load";
 rc_t CC UsageSummary ( const char * progname )
 {
     OUTMSG ( (
+        "Summary: Loads pacbio hd5 data into an SRA archive.\n"
         "Usage:\n"
         "  %s <hdf5-file> -o<target>\n"
         "\n", progname) );

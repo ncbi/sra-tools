8c1d35aff6e1dba384c1010052b2e87b40478f97
Rahim Rajwani <rajwanir2@gmail.com>
2026-05-28 17:03:24 -0400

    Resolves VDB-6357. Adds a summary section for sra-sort in CLI help message.

--- tools/external/sra-stat/sra-stat.c
+++ tools/external/sra-stat/sra-stat.c
@@ -4773,6 +4773,7 @@ OptDef Options[] = { /*                            maxCount needValue required*/
 rc_t CC UsageSummary (const char * progname)
 {
     return KOutMsg (
+        "Summary: Sorts an SRA archive by reference position - similar to samtools sort.\n"
         "Usage:\n"
         "  %s [options] table\n"
         "\n"

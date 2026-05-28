07ba35c0aba30eee14e5ab9779d7008cb20e1638
Rahim Rajwani <rajwanir2@gmail.com>
2026-05-28 16:48:15 -0400

    Resolves VDB-6350. Adds a summary section for fasterq-dump in CLI help message.

--- tools/external/fasterq-dump/fasterq-dump.c
+++ tools/external/fasterq-dump/fasterq-dump.c
@@ -296,6 +296,7 @@ const char UsageDefaultName[] = "fasterq-dump";
 
 rc_t UsageSummary( const char * progname ) {
     return KOutMsg(
+                     "Summary: A faster implementation of the fastq-dump. Extracts fastq from an SRA data file, only faster!\n"
                      "Usage:\n"
                      "  %s <path> [options]\n"
                      "  %s <accession> [options]\n"

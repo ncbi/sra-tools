7502303fae46cc6e9dc0ddb334977f13e5d46340
wraetz <raetzw@nih.gov>
2026-05-29 09:22:48 -0400

    VDB-6378 added test to verify that all 3 modes of rnd2sra work for fasterq-dump

---
+++ tools/test-tools/rnd2sra/test-vdb-6378/2.sh
@@ -0,0 +1,75 @@
+set -e
+
+ACC="2.acc"
+INI="2.ini"
+
+function run_and_check_flat {
+echo "CHECK FLAT"
+rm -rf $ACC
+echo "seed = 10101
+rows = 10
+product = flat
+checksum = md5
+with_name = yes
+layout = T5 : B30-50 : T5 : B44
+layout = T5 : B55 T5 : B32
+layout = T5 : B45 T5 : B47
+spotgroup = SG1
+spotgroup = SG2" > $INI
+rnd2sra $INI --out $ACC
+fasterq-dump $ACC
+vdb-dump $ACC -R1 -C BASE_COUNT
+echo "----------------------------------
+"
+}
+
+run_and_check_flat
+
+function run_and_check_db {
+echo "CHECK DB"
+rm -rf $ACC
+echo "seed = 10101
+rows = 10
+product = db
+checksum = md5
+with_name = yes
+layout = T5 : B30-50 : T5 : B44
+layout = T5 : B55 T5 : B32
+layout = T5 : B45 T5 : B47
+spotgroup = SG1
+spotgroup = SG2" > $INI
+rnd2sra $INI --out $ACC
+fasterq-dump $ACC
+vdb-dump $ACC -R1 -C BASE_COUNT
+echo "----------------------------------
+"
+}
+
+run_and_check_db
+
+function run_and_check_csra {
+echo "CHECK cSRA"
+rm -rf $ACC
+echo "seed = 10101
+rows = 10
+product = csra
+checksum = md5
+spots = F : 12 : 55 : 120
+spots = N : 7 : 50 : 100
+spots = 1 : 3 : 57 : 102
+spots = 2 : 4 : 56 : 103
+spotgroup = SG1
+spotgroup = SG2" > $INI
+rnd2sra $INI --out $ACC
+fasterq-dump $ACC
+vdb-dump $ACC -R1 -C BASE_COUNT
+vdb-dump $ACC -T PRIM -R1 -C BASE_COUNT
+echo "----------------------------------
+"
+}
+
+run_and_check_csra
+
+echo "success!"
+
+rm -rf *.fastq 2.acc 2.ini
